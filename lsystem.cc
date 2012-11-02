edde3732313443a3089ffe5163cd2ac9a178c8c2
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2012-11-02 23:48:13 +0100

    Quadratic Koch

--- lsystem.cc
+++ lsystem.cc
@@ -11,16 +11,23 @@ struct LSystem : Widget {
     int max=0;
     LSystem() {
         window.localShortcut(Escape).connect(&exit); window.backgroundCenter=window.backgroundColor=0xFF;
-        window.localShortcut(Key(' ')).connect([this]{max=min(8,max++); window.render();});
+        window.localShortcut(Key(' ')).connect([this]{max=(max+1)%8; window.render();});
     }
     void render(int2 position, int2 size) override {
         window.setTitle(dec(max));
-        array<vec2> polygon; polygon
-                << vec2(cos(1*PI/6),sin(1*PI/6))
-                << vec2(cos(5*PI/6),sin(5*PI/6))
-                << vec2(cos(9*PI/6),sin(9*PI/6))
-                << vec2(cos(1*PI/6),sin(1*PI/6));
-        array<vec2> generator; generator << vec2(0,0) << vec2(1./3,0) << vec2(1./3+cos(PI/3)/3,-sin(PI/3)/3) << vec2(2./3,0) << vec2(1,0);
+        array<vec2> polygon, generator;
+        if(0) { //Triangle Koch
+            polygon
+                    << vec2(cos(1*PI/6),sin(1*PI/6))
+                    << vec2(cos(5*PI/6),sin(5*PI/6))
+                    << vec2(cos(9*PI/6),sin(9*PI/6))
+                    << vec2(cos(1*PI/6),sin(1*PI/6));
+            generator << vec2(0,0) << vec2(1./3,0) << vec2(1./3+cos(PI/3)/3,-sin(PI/3)/3) << vec2(2./3,0) << vec2(1,0);
+        } else { //Quadratic Koch
+            polygon << vec2(-1,0) << vec2(1,0);
+            generator << vec2(0,0) << vec2(1./4,0) << vec2(1./4,1./4) << vec2(2./4,1./4) << vec2(2./4,0)
+                      << vec2(2./4,-1./4) << vec2(3./4,-1./4) << vec2(3./4,0) << vec2(1,0);
+        }
         for(int unused i: range(max)) {
             array<vec2> next;
             vec2 A=polygon.first();
