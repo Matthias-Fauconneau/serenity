c33a5b7debb86ebae4b2b039a28e16689973dbfb
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2013-12-01 15:52:37 +0100

    Synthesize axis aligned spheroids.

--- rock/synthetic.cc
+++ rock/synthetic.cc
@@ -14,30 +14,31 @@ class(Synthetic, Operation), virtual VolumeOperation {
         target.maximum = 0xFF;
         clear<uint8>(target, target.size(), uint8(0xFF));
         const uint maximumRadius = (size.x-1)/4;
-        struct Sphere { vec3 center; float radius; };
+        struct Sphere { vec3 center; vec3 radius; };
         array<Sphere> spheres;
         struct Tube { vec3 A, B; float radius; };
         array<Tube> tubes;
         for(Random random; spheres.size < (uint)size.x; ) {
-            float radius = 1+random()*(maximumRadius-1);
-            const vec3 margin = 1+radius;
+            vec3 radius = vec3(1)+vec3(random(),random(),random())*float(maximumRadius-1);
+            const vec3 margin = radius+vec3(1);
             vec3 center = margin+vec3(random(),random(),random())*(vec3(size) - 2.f*margin);
             Sphere nearest = {vec3(0),0}; float distance=size.x;
             for(const Sphere& o: spheres) {
-                if(norm(o.center-center)<o.radius+radius) goto break_; // Prevents intersections
+                if(abs(o.center-center) <= o.radius+radius) goto break_; // Prevents intersections (FIXME: box)
                 if(norm(o.center-center)<distance) distance=norm(o.center-center), nearest = o;
             }
             /*else*/ {
                 { // Rasterizes spheres (pores)
-                    int3 min=clip(int3(0),int3(center)-int3(radius),target.sampleCount), max=clip(int3(0),int3(ceil(center))+int3(radius),target.sampleCount);
+                    int3 min =clip(int3(0),int3(floor(center))- int3(ceil(radius)),target.sampleCount);
+                    int3 max=clip(int3(0),int3(ceil  (center))+int3(ceil(radius)),target.sampleCount);
                     for(int z: range(min.z, max.z)) for(int y: range(min.y, max.y)) for(int x: range(min.x, max.x)) {
-                        if(sq(vec3(x,y,z)-center) <= sq(radius)) target(x,y,z) = 0;
+                        if(sq((vec3(x,y,z)-center)/radius) <= 1) target(x,y,z) = 0;
                     }
                     spheres << Sphere{center, radius};
                 }
-                if(nearest.radius) { // Rasterizes tubes (throats)
+                /*if(nearest.radius) { // Rasterizes tubes (throats)
                     vec3 A = center, B = nearest.center;
-                    float tubeRadius = min(radius, nearest.radius)/2;
+                    float tubeRadius = min(norm(radius), norm(nearest.radius))/2;
                     int3 min=clip(int3(0),int3(::min(A,B))-int3(radius),target.sampleCount);
                     int3 max=clip(int3(0),int3(ceil(::max(A,B)))+int3(radius),target.sampleCount);
                     for(int z: range(min.z, max.z)) for(int y: range(min.y, max.y)) for(int x: range(min.x, max.x)) {
@@ -48,7 +49,7 @@ class(Synthetic, Operation), virtual VolumeOperation {
                         if(sq(P - (A+t*(B-A))) < sq(tubeRadius)) target(x,y,z) = 0; // Straight cylinder
                     }
                     tubes << Tube{A, B, tubeRadius};
-                }
+                }*/
             }
             break_:;
         }
