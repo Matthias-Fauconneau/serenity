8f633861e0a77c8fda0fcb72c8992503f811416d
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2014-03-06 10:18:07 +0100

    install.sh: Removes unused "release" build.

--- install.sh
+++ install.sh
@@ -2,6 +2,5 @@ PREFIX=$1
 test -z $PREFIX && export PREFIX=/pool/users/$USER
 rm $PREFIX/rock.fast -f && ./build.sh rock fast $PREFIX && chmod +x $PREFIX/rock.fast &&
 rm $PREFIX/rock.debug -f && ./build.sh rock debug $PREFIX  && chmod +x $PREFIX/rock.debug &&
-#rm $PREFIX/rock.release -f && ./build.sh rock release $PREFIX && chmod +x $PREFIX/rock.release &&
 cp rock/rock $PREFIX/rock.process &&
 ./test.sh
