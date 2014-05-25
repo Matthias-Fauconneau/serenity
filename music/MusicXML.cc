5cf7bc0d1bce1aae5d86224302b7ab4254dd1546
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2014-05-25 21:56:34 +1000

    Fixes.

--- music/MusicXML.cc
+++ music/MusicXML.cc
@@ -8,10 +8,10 @@ array<Sign> parse(string document, uint& divisions) {
     uint measureIndex=1;
     for(const Element& m: root("score-partwise"_)("part"_).children) {
         assert_(m.name=="measure"_, m);
-        log("Measure:", measureIndex);
+        //log("Measure:", measureIndex);
         for(const Element& e: m.children) {
             if(!(e.name=="note"_ && e("chord"_))) time = nextTime; // Advances time (except chords)
-            log("time", time%(timeSignature.beats*divisions));
+            //log("time", time%(timeSignature.beats*divisions));
             maxTime = max(maxTime, time);
 
             if(e.name=="note"_) {
@@ -20,7 +20,7 @@ array<Sign> parse(string document, uint& divisions) {
                 if(!e("chord"_)) nextTime = time+duration;
                 if(e["print-object"_]=="no"_) continue;
                 uint staff = fromInteger(e("staff"_).text())-1;
-                Duration type = Duration(ref<string>{"whole"_,"half"_,"quarter"_,"eighth"_,"16th"_}.indexOf(e("type"_).text()));
+                Duration type = Duration(ref<string>({"whole"_,"half"_,"quarter"_,"eighth"_,"16th"_}).indexOf(e("type"_).text()));
                 assert_(int(type)>=0, e);
                 if(e("rest"_)) {
                     {Sign sign{time, duration, staff, Sign::Rest, {}}; sign.rest={type}; signs.insertSorted(sign);}
@@ -29,7 +29,7 @@ array<Sign> parse(string document, uint& divisions) {
                     uint octaveStep = "CDEFGAB"_.indexOf(e("pitch"_)("step"_).text()[0]);
                     int octave = fromInteger(e("pitch"_)("octave"_).text());
                     int step = (octave-4) * 7 + octaveStep;
-                    Accidental accidental = Accidental(ref<string>{""_,"flat"_,"sharp"_,"natural"_}.indexOf(e("accidental"_).text()));
+                    Accidental accidental = Accidental(ref<string>({""_,"flat"_,"sharp"_,"natural"_}).indexOf(e("accidental"_).text()));
                     if(e("notations"_)("slur"_)) {
                         if(slurs[staff]) assert_(e("notations"_)("slur"_).attribute("type"_)=="stop"_);
                         else assert_(e("notations"_)("slur"_).attribute("type"_)=="start"_, e("notations"_)("slur"_).attribute("type"_), e);
@@ -51,35 +51,35 @@ array<Sign> parse(string document, uint& divisions) {
             else if(e.name=="backup"_) {
                 int dt = fromInteger(e("duration"_).text());
                 time -= dt;
-                log("<<", dt);
+                //log("<<", dt);
                 nextTime = time;
             }
             else if(e.name=="forward"_) {
                 int dt =  fromInteger(e("duration"_).text());
                 time += dt;
-                log(">>", dt);
-                //maxTime = max(maxTime, time);
+                //log(">>", dt);
+                maxTime = max(maxTime, time);
                 nextTime = time;
             }
             else if(e.name=="direction"_) {
                 const Element& d = e("direction-type"_);
                 if(d("dynamics"_)) {
-                    Loudness loudness = Loudness(ref<string>{"ppp"_,"pp"_,"p"_,"mp"_,"mf"_,"f"_,"ff"_,"fff"_}.indexOf(d("dynamics"_).children.first()->name));
+                    Loudness loudness = Loudness(ref<string>({"ppp"_,"pp"_,"p"_,"mp"_,"mf"_,"f"_,"ff"_,"fff"_}).indexOf(d("dynamics"_).children.first()->name));
                     {Sign sign{time, 0, 0, Sign::Dynamic, {}}; sign.dynamic={loudness}; signs << sign;}
                 }
                 else if(d("metronome"_)) {
-                    Duration beatUnit = Duration(ref<string>{"whole"_,"half"_,"quarter"_,"eighth"_,"16th"_}.indexOf(d("metronome"_)("beat-unit"_).text()));
+                    Duration beatUnit = Duration(ref<string>({"whole"_,"half"_,"quarter"_,"eighth"_,"16th"_}).indexOf(d("metronome"_)("beat-unit"_).text()));
                     uint perMinute = fromInteger(d("metronome"_)("per-minute"_).text());
                     {Sign sign{time, 0, 0, Sign::Metronome, {}}; sign.metronome={beatUnit, perMinute}; signs << sign;}
                 }
                 else if(d("pedal"_)) {
-                    PedalAction action = PedalAction(ref<string>{"start"_,"change"_,"stop"_}.indexOf(d("pedal"_)["type"_]));
+                    PedalAction action = PedalAction(ref<string>({"start"_,"change"_,"stop"_}).indexOf(d("pedal"_)["type"_]));
                     if(action==Start && d("pedal"_)["line"_]!="yes"_) action=Ped;
                     int offset = e("offset"_) ? fromInteger(e("offset"_).text()) : 0;
                     {Sign sign{time + offset, 0, 0, Sign::Pedal, {}}; sign.pedal={action}; signs.insertSorted(sign);}
                 }
                 else if(d("wedge"_)) {
-                    WedgeAction action = WedgeAction(ref<string>{"crescendo"_,"diminuendo"_,"stop"_}.indexOf(d("wedge"_)["type"_]));
+                    WedgeAction action = WedgeAction(ref<string>({"crescendo"_,"diminuendo"_,"stop"_}).indexOf(d("wedge"_)["type"_]));
                     {Sign sign{time, 0, 0, Sign::Wedge, {}}; sign.wedge={action}; signs << sign;}
                 }
                 else if(d("octave-shift"_)) {}
@@ -109,12 +109,12 @@ array<Sign> parse(string document, uint& divisions) {
         }
         time=nextTime;
         maxTime = max(maxTime, time);
-        //time=maxTime;
+        time=maxTime;
         measureIndex++;
         {Sign sign{time, 0, 0, Sign::Measure, {}}; sign.measure.index=measureIndex; signs.insertSorted(sign);}
         {Sign sign{time, 0, 1, Sign::Measure, {}}; sign.measure.index=measureIndex; signs.insertSorted(sign);}
-        if(time%(timeSignature.beats*divisions)!=0) break;
-        assert_(time%(timeSignature.beats*divisions)==0, measureIndex, time, timeSignature.beats, divisions);
+        //if(time%(timeSignature.beats*divisions)!=0) break;
+        //assert_(time%(timeSignature.beats*divisions)==0, measureIndex, time, timeSignature.beats, divisions);
     }
     return signs;
 }
