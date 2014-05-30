43e190374d2098591a5d5edb175e72ad372af6e8
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2014-05-30 16:06:22 +1000

    Restore OpenCL renderer.

---
+++ core/opencl.cc
@@ -0,0 +1,49 @@
+#include "opencl.h"
+
+cl_context context;
+cl_command_queue queue;
+cl_device_id device;
+
+void __attribute((constructor(1002))) setup_cl() {
+    uint platformCount; clGetPlatformIDs(0, 0, &platformCount);
+    cl_platform_id platforms[platformCount]; clGetPlatformIDs(platformCount, platforms, 0);
+    for(cl_platform_id platform: ref<cl_platform_id>(platforms, platformCount)) {
+        for(cl_platform_info attribute : { CL_PLATFORM_NAME, CL_PLATFORM_VENDOR, CL_PLATFORM_VERSION, CL_PLATFORM_PROFILE, CL_PLATFORM_EXTENSIONS }) {
+            size_t size; clGetPlatformInfo(platform, attribute, 0, 0, &size);
+            char info[size]; clGetPlatformInfo(platform, attribute, size, info, 0);
+            log(string(info,size-1));
+        }
+    }
+    cl_platform_id platform = platforms[0]; // {AMD, Intel, Beignet}
+    uint deviceCount; clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, 0, 0, &deviceCount);
+    cl_device_id devices[deviceCount]; clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, deviceCount, devices, 0);
+    for(cl_device_id device: ref<cl_device_id>(devices, deviceCount)) {
+        for(cl_device_info attribute : { CL_DEVICE_NAME, CL_DEVICE_VERSION, CL_DRIVER_VERSION}) {
+            size_t size; clGetDeviceInfo(device, attribute, 0, 0, &size);
+            char info[size]; clGetDeviceInfo(device, attribute, size, info, 0);
+            log(string(info,size-1));
+        }
+    }
+    device = devices[1]; // {GPU, CPU}
+    context = clCreateContext(0, 1, &device, &clNotify, 0, 0);
+    queue = clCreateCommandQueue(context, device, 0, 0);
+}
+
+cl_kernel createKernel(string source, string name) {
+    int status;
+    cl_program program = clCreateProgramWithSource(context, 1, &source.data, &source.size, &status);
+    if(clBuildProgram(program, 0, 0, 0, 0, 0)) {
+        size_t buildLogSize;
+        clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, 0, 0, &buildLogSize);
+        char buildLog[buildLogSize];
+        clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, buildLogSize, (void*)buildLog, 0);
+        array<string> lines = split(string(buildLog,buildLogSize-1),'\n');
+        log(join(lines.slice(0,min(16ul,lines.size)),"\n"_));
+        error(__FILE__);
+    }
+    assert_(name.last()==0);
+    cl_kernel kernel = clCreateKernel(program, name.data, &status);
+    clCheck(status);
+    assert_(kernel);
+    return kernel;
+}
---
+++ core/opencl.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <CL/opencl.h> //OpenCL
+#include "string.h"
+
+inline void clNotify(const char* info, const void *, size_t, void *) { error(info); }
+
+static string clErrors[] = {"CL_SUCCESS"_, "CL_DEVICE_NOT_FOUND"_, "CL_DEVICE_NOT_AVAILABLE"_, "CL_COMPILER_NOT_AVAILABLE"_, "CL_MEM_OBJECT_ALLOCATION_FAILURE"_, "CL_OUT_OF_RESOURCES"_, "CL_OUT_OF_HOST_MEMORY"_, "CL_PROFILING_INFO_NOT_AVAILABLE"_, "CL_MEM_COPY_OVERLAP"_, "CL_IMAGE_FORMAT_MISMATCH"_, "CL_IMAGE_FORMAT_NOT_SUPPORTED"_, "CL_BUILD_PROGRAM_FAILURE"_, "CL_MAP_FAILURE"_, ""_, ""_, ""_, ""_, ""_, ""_, ""_, ""_, ""_, ""_, ""_, ""_, ""_, ""_, ""_, ""_, ""_, "CL_INVALID_VALUE"_, "CL_INVALID_DEVICE_TYPE"_, "CL_INVALID_PLATFORM"_, "CL_INVALID_DEVICE"_, "CL_INVALID_CONTEXT"_, "CL_INVALID_QUEUE_PROPERTIES"_, "CL_INVALID_COMMAND_QUEUE"_, "CL_INVALID_HOST_PTR"_, "CL_INVALID_MEM_OBJECT"_, "CL_INVALID_IMAGE_FORMAT_DESCRIPTOR"_, "CL_INVALID_IMAGE_SIZE"_, "CL_INVALID_SAMPLER"_, "CL_INVALID_BINARY"_, "CL_INVALID_BUILD_OPTIONS"_, "CL_INVALID_PROGRAM"_, "CL_INVALID_PROGRAM_EXECUTABLE"_, "CL_INVALID_KERNEL_NAME"_, "CL_INVALID_KERNEL_DEFINITION"_, "CL_INVALID_KERNEL"_, "CL_INVALID_ARG_INDEX"_, "CL_INVALID_ARG_VALUE"_, "CL_INVALID_ARG_SIZE"_, "CL_INVALID_KERNEL_ARGS"_, "CL_INVALID_WORK_DIMENSION"_, "CL_INVALID_WORK_GROUP_SIZE"_, "CL_INVALID_WORK_ITEM_SIZE"_, "CL_INVALID_GLOBAL_OFFSET"_, "CL_INVALID_EVENT_WAIT_LIST"_, "CL_INVALID_EVENT"_, "CL_INVALID_OPERATION"_, "CL_INVALID_GL_OBJECT"_, "CL_INVALID_BUFFER_SIZE"_, "CL_INVALID_MIP_LEVEL"_, "CL_INVALID_GLOBAL_WORK_SIZE"_};
+#define clCheck( expr) ({ int _status = expr; assert_(!_status, clErrors[-_status], #expr); })
+
+extern cl_context context;
+extern cl_command_queue queue;
+
+cl_kernel createKernel(string source, string name);
+
+#define KERNEL(name) static cl_kernel name() { \
+    extern char _binary_ ## name ##_cl_start[], _binary_ ## name ##_cl_end[]; \
+    return createKernel(ref<byte>(_binary_ ## name ##_cl_start,_binary_ ## name ##_cl_end), #name); \
+}
+
+typedef float float2 __attribute__((ext_vector_type(2)));
+typedef float float3 __attribute__((ext_vector_type(3)));
+//typedef float float4 __attribute__((ext_vector_type(4)));
