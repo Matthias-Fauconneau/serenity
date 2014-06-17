dc8c4e51ec9b8b2c2acba1e9b0e7dbb8f673dc5c
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2014-06-18 08:45:55 +1000

    Note highlighting.

--- music/music.cc
+++ music/music.cc
@@ -11,12 +11,47 @@ struct Music : Poll {
     Sheet sheet {xml.signs, xml.divisions};
     buffer<uint> midiToBlit = sheet.synchronize(apply(midi.notes,[](MidiNote o){return o.key;}));
     Encoder encoder {name /*, {&audioFile, &AudioFile::read}*/}; //TODO: remux without reencoding
-    Window window {&sheet, encoder.size(), "MusicXML"_};
+    bool preview = true;
+    Time time;
+    Window window {&sheet, encoder.size()/(preview?2:1), "MusicXML"_};
     Image image;
     bool contentChanged = true;
+
+    map<uint,uint> active; // Maps active keys to notes (indices)
+    map<uint,uint> expected; // Maps expected keys to notes (indices)
+    uint chordIndex = 0;
+
+    void expect() {
+        while(!expected && chordIndex<sheet.notes.size()-1) {
+            const array<Note>& chord = sheet.notes.values[chordIndex];
+            for(uint i: range(chord.size)) {
+                uint key = chord[i].key, index = chord[i].blitIndex;
+                if(!expected.contains(key)) {
+                    expected.insert(key, index);
+                    //errors = 0; showExpected = false; // Hides highlighting while succeeding
+                }
+            }
+            chordIndex++;
+        }
+    }
+
     Music() {
-        midi.noteEvent.connect([this](const uint unused key, const uint unused vel){
-            // TODO: highlight notes
+        expect();
+        midi.noteEvent.connect([this](const uint key, const uint vel){
+            if(vel) {
+                if(expected.contains(key)) {
+                    active.insertMulti(key,expected.at(key));
+                    expected.remove(key);
+                }
+                //else if(!showExpected) { errors++; if(errors>1) showExpected = true; } // Shows expected notes on errors (allows one error before showing)
+                else return; // No changes
+            } else if(key) {
+                if(active.contains(key)) while(active.contains(key)) active.remove(key);
+                else return; // No changes
+            }
+            expect();
+            sheet.colors.clear();
+            for(uint index: active.values) sheet.colors.insert(index, red);
             contentChanged = true;
         });
 
@@ -28,17 +63,16 @@ struct Music : Poll {
     }
     void event() {
         if(midi.time > midi.duration) return;
-        midi.read(encoder.videoTime*encoder.rate/encoder.fps);
-        int position = sheet.measures.last() * midi.time / midi.duration;
+        midi.read(preview ? time*48 : encoder.videoTime*encoder.rate/encoder.fps);
+        int position = int64(sheet.measures.last()-window.size.x) * int64(midi.time-midi.tracks[0].startTime) / int64(midi.duration);
         if(sheet.position != position) sheet.position = position, contentChanged = true;
         if(contentChanged) {
             window.render(); window.event();
             image = share(window.target);
             assert_(image);
-            //image = renderToImage(layout, encoder.size());
             contentChanged=false;
         }
-        encoder.writeVideoFrame(image);
+        if(!preview) encoder.writeVideoFrame(image);
         queue();
     }
 } app;
