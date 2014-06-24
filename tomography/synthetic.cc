1655e6c9d29a65e417425c70a3241a2d80d16fba
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2014-06-24 13:39:05 +1000

    Optimize projection.

--- tomography/synthetic.cc
+++ tomography/synthetic.cc
@@ -6,18 +6,23 @@
 #include "text.h"
 #include "layout.h"
 #include "window.h"
+#include "view.h"
+
+const float airDensity = 0.001; // Houndsfield ?
+const float containerDensity = 5.6; // Pure iron
+const float maximumRadius = 32; // vx
+const float rate = 1./cb(maximumRadius); // 1/vx
+const struct GrainType { const float probability; /*relative concentration*/ const float density; } types[] = {/*Rutile*/{0.7, 4.20}, /*Siderite*/{0.2, 3.96}, /*NaMontmorillonite*/{0.1, 2.65}};
 
 VolumeF porousRock(int3 size) {
     assert(size.x == size.y);
 
-    const float airDensity = 0.001; // Houndsfield ?
     VolumeF volume (size, airDensity);
 
     const vec2 center = vec2(size.xy()-int2(1))/2.f;
     const float volumeRadius = center.x;
     const float innerRadius = (1-4./100) * volumeRadius;
     const float outerRadius = (1-2./100) * volumeRadius;
-    const float containerDensity = 5.6; // Pure iron
     for(uint y: range(size.y)) for(uint x: range(size.x)) {
         float r2 = sq(vec2(x,y)-vec2(size.xy()-1)/2.f);
         if(sq(innerRadius-1./2) < r2 && r2 < sq(outerRadius+1./2)) {
@@ -35,10 +40,7 @@ VolumeF porousRock(int3 size) {
         }
     }
 
-    const struct GrainType { const float probability; /*relative concentration*/ const float density; } types[] = {/*Rutile*/{0.7, 4.20}, /*Siderite*/{0.2, 3.96}, /*NaMontmorillonite*/{0.1, 2.65}};
     assert(sum(apply(ref<GrainType>(types), [](GrainType t){return t.probability;})) == 1);
-    const float maximumRadius = 16; // vx
-    const float rate = 1./cb(maximumRadius); // 1/vx
     const uint grainCount = rate*size.z*size.y*size.x;
     Random random; // Unseeded sequence (for repeatability)
     Time time;
@@ -46,12 +48,15 @@ VolumeF porousRock(int3 size) {
         float* volumeData = volume.data;
         const uint XY = size.x*size.x;
         const uint X = size.x;
-        for(uint count=0; count < type.probability * grainCount;) {
-            const float r = random()*maximumRadius; // Uniform distribution of radius between [0, maximumRadius[
-            const vec2 xy = vec2(random(), random()) * vec2(size.xy());
+        for(uint count=0; count < type.probability * grainCount; count++) {
+            const float r0 = random();
+            const float r1 = random();
+            const float r2 = random();
+            const float r3 = random();
+            const float r = r0*maximumRadius; // Uniform distribution of radius between [0, maximumRadius[
+            const vec2 xy = vec2(r1, r2) * vec2(size.xy());
             if(sq(xy-vec2(volumeRadius)) > sq(innerRadius-r)) continue;
-            count++;
-            float cx = xy.x, cy=xy.y, cz=r+random()*((size.z-1)-2*r);
+            float cx = xy.x, cy=xy.y, cz=r+r3*((size.z-1)-2*r);
             float innerR2 = sq(r-1.f/2);
             float outerRadius = r+1.f/2;
             float outerR2 = sq(outerRadius);
@@ -125,38 +130,23 @@ struct Intersection { float t; int index; };
 inline bool operator <(const Intersection& a, const Intersection& b) { return a.t < b.t /*|| (a.t == b.t && a.density > b.density)*/; } // Sort by t /*, push before pop*/ checked before insert
 inline String str(const Intersection& a) { return str(a.t, a.index); }
 
-void getBoundsForAxis(const vec3 a, const vec3 center, float radius, vec3& U, vec3& L){
-    //log("a",a, "center",center, "radius",radius);
-    const vec2 projectedCenter = vec2(dot(a, center), center.z); // Project center from xyz to az
-    //log("projectedCenter",projectedCenter);
-    //assert_(sq(projectedCenter) > sq(radius), center, projectedCenter, radius); // Camera outside sphere
+void getBoundsForAxis(const vec2 a, const vec3 center, float radius, vec3& U, vec3& L){
+    const vec2 projectedCenter = vec2(dot(a, center.xy()), center.z); // Project center from xyz to az
     float t = sqrt(sq(projectedCenter) - sq(radius)); // Hypothenuse length (distance to the tangent points of the sphere (points where a vector from the camera are tangent to the sphere) (calculated a-z space))
-    //log("t",t);
     float cLength = norm(projectedCenter);
-    //log("cLength",cLength);
-    // Theta is the angle between the vector from the camera to the center of the sphere and the vectors from the camera to the tangent points
     float costheta = t / cLength;
     float sintheta = radius / cLength;
-    //log("costheta", costheta, "sintheta", sintheta);
-    vec2 bounds_az[2];
-    for(int i = 0; i<2; i++) {
-        mat2 rotateTheta = mat2(costheta, -sintheta, sintheta, costheta);
-        bounds_az[i] = costheta * (rotateTheta * projectedCenter);
-        sintheta *= -1; // negate theta for B
-    }
-    U   = bounds_az[0].x * a;
-    U.z = bounds_az[0].y;
-    //log("U", U);
-    L   = bounds_az[1].x * a;
-    L.z = bounds_az[1].y;
-    //log("L", L);
+    vec2 bounds_az0 = costheta * (mat2(costheta, -sintheta, sintheta, costheta) * projectedCenter);
+    U = vec3(bounds_az0.x * a.x, bounds_az0.x * a.y, bounds_az0.y);
+    vec2 bounds_az1 = costheta * (mat2(costheta, sintheta, -sintheta, costheta) * projectedCenter);
+    L = vec3(bounds_az1.x * a.x, bounds_az1.x * a.y, bounds_az1.y);
 }
 
 struct RectF { vec2 min, max; };
 RectF getBoundingBox(const vec3& center, float radius, const mat4& projMatrix) {
     vec3 maxXHomogenous, minXHomogenous, maxYHomogenous, minYHomogenous;
-    getBoundsForAxis(vec3(1, 0, 0),  center, radius, maxXHomogenous, minXHomogenous);
-    getBoundsForAxis(vec3(0, 1, 0), center, radius, maxYHomogenous, minYHomogenous);
+    getBoundsForAxis(vec2(1, 0),  center, radius, maxXHomogenous, minXHomogenous);
+    getBoundsForAxis(vec2(0, 1), center, radius, maxYHomogenous, minYHomogenous);
     // We only need one coordinate for each point, so we save computation by only calculating x(or y) and w
     float maxX = dot(maxXHomogenous, projMatrix.row(0)) / dot(maxXHomogenous, projMatrix.row(3));
     float minX = dot(minXHomogenous, projMatrix.row(0)) / dot(minXHomogenous, projMatrix.row(3));
@@ -166,12 +156,13 @@ RectF getBoundingBox(const vec3& center, float radius, const mat4& projMatrix) {
 }
 
 void porousRock(const ImageF& target, const Projection& A, uint index) {
+    Time totalTime;
     const vec3 volumeCenter = vec3(A.volumeSize-int3(1))/2.f;
     const float volumeRadius = volumeCenter.x;
     const float innerRadius = (1-4./100) * volumeRadius;
     const struct GrainType { const float probability; /*relative concentration*/ const float density; } types[] = {/*Rutile*/{0.7, 4.20}, /*Siderite*/{0.2, 3.96}, /*NaMontmorillonite*/{0.1, 2.65}};
     assert(sum(apply(ref<GrainType>(types), [](GrainType t){return t.probability;})) == 1);
-    const float maximumRadius = 16; // vx
+    const float maximumRadius = 32; // vx
     const float rate = 1./cb(maximumRadius); // 1/vx
     int3 size = A.volumeSize;
     const uint grainCount = rate*size.z*size.y*size.x;
@@ -187,83 +178,95 @@ void porousRock(const ImageF& target, const Projection& A, uint index) {
     //log(worldToView, viewOrigin);
     uint typeCount = ref<GrainType>(types).size;
     Random random; // Unseeded sequence (for repeatability)
-    buffer<array<Intersection>> intersections (target.size.y*target.size.x);
-    for(array<Intersection>& list: intersections) new (&list) array<Intersection>(2*grainCount); // Conservative bound on intersection count
+    buffer<float> randomSeq (grainCount*4 *2 /*Just to be sure*/);
+    for(float& v: randomSeq) v= random(); // Cannot be kept consistent and parallelized (FIXME: do once)
+    uint randomIndex = 0;
+    static buffer<buffer<Intersection>> intersections (target.size.y*target.size.x, target.size.y*target.size.x, 2*grainCount);
+    for(auto& e: intersections) e.size = 0;
+    Time rasterizationTime;
     for(uint index: range(typeCount)) { // Rasterizes each grain type in order so that lighter grains overrides heavier grains
         GrainType type = types[index];
-        for(uint count=0; count < type.probability * grainCount;) {
-            const float radius = random()*maximumRadius; // Uniform distribution of radius between [0, maximumRadius[
-            const vec2 xy = vec2(random(), random()) * vec2(size.xy());
-            if(sq(xy-vec2(volumeRadius)) > sq(innerRadius-radius)) continue;
-            count++;
-            vec3 C(xy.x, xy.y, radius+random()*((size.z-1)-2*radius));
+        //for(uint count=0; count < type.probability * grainCount;) {
+        uint count = type.probability * grainCount;
+        const float* random = randomSeq.data + randomIndex;
+        parallel(count, [&](const uint, const uint grainIndex) {
+            const float radius = random[grainIndex*4+0]*maximumRadius; // Uniform distribution of radius between [0, maximumRadius[
+            const vec2 xy = vec2(random[grainIndex*4+1], random[grainIndex*4+2]) * vec2(size.xy());
+            if(sq(xy-vec2(volumeRadius)) > sq(innerRadius-radius)) return;
+            vec3 C(xy.x, xy.y, radius+random[grainIndex*4+3]*((size.z-1)-2*radius));
             C -= volumeCenter; // Translates from data [0..size] to world [±size]
             vec3 vC = worldToView * C; // Transforms from world to homogeneous view coordinates
-            //C = -C.z; // Look towards negative Z
             RectF r = getBoundingBox(vC, radius, projectionMatrix);
             r.min += imageCenter, r.max += imageCenter;
-            const vec4 pC = projectionMatrix*vec4(vC,1);
-            const vec2 pCxy = imageCenter+pC.xy()/pC.w;
-            //log("min", r.min, "center", pCxy, "max", r.max);
-            //assert_(r.min <= pCxy && pCxy <= r.max, r.min, pCxy, r.max);
             for(uint y: range(max(0,int(floor(r.min.y))), min(target.size.y, int(ceil(r.max.y))))) {
                 for(uint x: range(max(0,int(floor(r.min.x))), min(target.size.x, int(ceil(r.max.x))))) {
-            //log(C, viewOrigin, C-viewOrigin);
-            //for(uint y: range(target.size.y)) { for(uint x: range(target.size.x)) {
-                    //const float3 ray = normalize(float(x) * imageToWorld[0].xyz() + float(y) * imageToWorld[1].xyz() + imageToWorld[2].xyz());
                     const float3 ray = normalize( (x-float(target.size.x-1)/2) * viewToWorld[0].xyz() + (y-float(target.size.y-1)/2) * viewToWorld[1].xyz() + 1.f * viewToWorld[2].xyz() ); // FIXME: store view->image translation in viewToWorld[2] (as in project.cl)
                     float tmin, tmax;
                     if(intersects(radius, C-viewOrigin, ray, tmin, tmax) && tmax>tmin /*i.e tmax != tmin*/) {
-                        intersections[y*target.size.x+x].insertSorted( Intersection{tmin, int(1+index)} );
-                        intersections[y*target.size.x+x].insertSorted( Intersection{tmax, -int(1+index)} );
+                        buffer<Intersection>& list = intersections[y*target.size.x+x];
+                        uint i = __sync_fetch_and_add(&list.size, 2);
+                        list[i] = Intersection{tmin, int(1+index)};
+                        list[i+1] = Intersection{tmax, -int(1+index)};
                     }
                 }
             }
-        }
+        });
+        randomIndex += count * 4;
     }
-
-    const float airDensity = 0.001; // Houndsfield ?
+    rasterizationTime.stop();
+    Time integrationTime;
     const float halfHeight = (A.volumeSize.z-1)/2;
     const float outerRadius = (1-2./100) * volumeRadius;
-    const float containerDensity = 5.6; // Pure iron
+    parallel(target.size.y, [&](const uint, const uint y) {
+        int counts[typeCount]; mref<int>(counts,typeCount).clear(0);
+        for(uint x: range(target.size.x)) {
+            const float3 ray = normalize( (x-float(target.size.x-1)/2) * viewToWorld[0].xyz() + (y-float(target.size.y-1)/2) * viewToWorld[1].xyz() + 1.f * viewToWorld[2].xyz()); // FIXME: store view->image translation in viewToWorld[2] (as in project.cl)
+            float densityRayIntegral = 0;
+            float outer[2]; intersects(halfHeight, outerRadius, viewOrigin, ray, outer[0], outer[1]);
+            float inner[2]; intersects(halfHeight, innerRadius, viewOrigin, ray, inner[0], inner[1]);
 
-    int counts[typeCount]; mref<int>(counts,typeCount).clear(0);
-    for(uint y: range(target.size.y)) for(uint x: range(target.size.x)) {
-        //const float3 ray = normalize(float(x) * imageToWorld[0].xyz() + float(y) * imageToWorld[1].xyz() + imageToWorld[2].xyz());
-        const float3 ray = normalize( (x-float(target.size.x-1)/2) * viewToWorld[0].xyz() + (y-float(target.size.y-1)/2) * viewToWorld[1].xyz() + 1.f * viewToWorld[2].xyz()); // FIXME: store view->image translation in viewToWorld[2] (as in project.cl)
-        float densityRayIntegral = 0;
-        float outer[2]; intersects(halfHeight, outerRadius, viewOrigin, ray, outer[0], outer[1]);
-        float inner[2]; intersects(halfHeight, innerRadius, viewOrigin, ray, inner[0], inner[1]);
-        if(outer[0]<inf) {
-            float length = inner[0] - outer[0];
-            if(length>=0 && length<inf) //assert_(length>=0, length, inner[0], outer[0], "0"); //FIXME
-                densityRayIntegral += length * containerDensity;
-        }
-        if(outer[1]>-inf) {
-            float length = outer[1] - inner[1];
-            if(length>=0 && length<inf) //assert_(length>=0, length, outer[0], inner[0], inner[1], outer[1], "1"); //FIXME
-                densityRayIntegral += length * containerDensity;
-        }
-        if(inner[0]<inf && inner[1]>-inf) {
-            float lastT = inner[0];
-            for(const Intersection& intersection: intersections[y*target.size.x+x]) {
-                float t = intersection.t;
-                float length = t - lastT;
-                lastT = t;
-                if(length > 0) {
-                    float density = airDensity;
-                    for(uint type: range(typeCount)) if(counts[type]) density = types[type].density; // In order so that lighter grains overrides heavier grains
-                    densityRayIntegral += length * density;
+            if(inner[0]<inf) {
+                assert_(inner[1]>-inf);
+                {assert_(outer[0]<inf);
+                float length = inner[0] - outer[0];
+                assert_(length>=0, length, inner[0], outer[0], "0"); //FIXME
+                if(length>=0)
+                    densityRayIntegral += length * containerDensity;}
+                assert_(outer[1]>-inf);
+                {float length = outer[1] - inner[1];
+                //assert_(length>=0 && length<inf, length, outer[0], inner[0], inner[1], outer[1], "1"); //FIXME
+                    if(length>0) densityRayIntegral += length * containerDensity;}
+
+                float lastT = inner[0];
+                quicksort(intersections[y*target.size.x+x]);
+                for(const Intersection& intersection: intersections[y*target.size.x+x]) {
+                    float t = intersection.t;
+                    float length = t - lastT;
+                    lastT = t;
+                    if(length > 0) {
+                        float density = airDensity;
+                        for(uint type: range(typeCount)) if(counts[type]) density = types[type].density; // In order so that lighter grains overrides heavier grains
+                        densityRayIntegral += length * density;
+                    }
+                    int index =  intersection.index;
+                    if(index > 0) counts[index-1]++;
+                    if(index < 0) counts[-index-1]--;
                 }
-                int index =  intersection.index;
-                if(index > 0) counts[index-1]++;
-                if(index < 0) counts[-index-1]--;
+                float length = inner[1] - lastT;
+                densityRayIntegral += length * airDensity;
+            } else if(outer[0]<inf) {
+                assert_(outer[1]>-inf);
+                float length = outer[1] - outer[0];
+                assert_(length>=0, length, outer[1], outer[0], "outer"); //FIXME
+                if(length>=0)
+                    densityRayIntegral += length * containerDensity;
             }
-            float length = inner[1] - lastT;
-            densityRayIntegral += length * airDensity;
+            target(x,y) = densityRayIntegral / A.volumeSize.x;
         }
-        target(x,y) = densityRayIntegral / A.volumeSize.x;
-    }
+    });
+    integrationTime.stop();
+    totalTime.stop();
+    //log(rasterizationTime, integrationTime, totalTime);
 }
 
 struct Analytic : Widget {
@@ -288,15 +291,15 @@ struct Analytic : Widget {
 };
 
 const int N = fromInteger(arguments()[0]);
-//VolumeF hostVolume = normalize(porousRock(N));
-//CLVolume volume {hostVolume};
-//SliceView sliceView {volume, 512/N};
+VolumeF hostVolume = normalize(porousRock(N));
+CLVolume volume {hostVolume};
+SliceView sliceView {volume, 512/N};
 Projection A {N, N};
 Value projectionIndex {20};
-//VolumeView volumeView {volume, A, 512/N, projectionIndex};
+VolumeView volumeView {volume, A, 512/N, projectionIndex};
 Analytic analyticView {A, 512/N, projectionIndex};
-HBox layout {{ /*&sliceView, &volumeView,*/ &analyticView }};
-/*struct App {
+HBox layout {{ &sliceView, &volumeView, &analyticView }};
+struct App {
     App() {
         writeFile("Data/"_+strx(hostVolume.size)+".ref"_, cast<byte>(hostVolume.data));
         VolumeF projectionData (A.projectionSize);
@@ -307,5 +310,5 @@ HBox layout {{ /*&sliceView, &volumeView,*/ &analyticView }};
         }
         writeFile("Data/"_+strx(projectionData.size)+".proj"_, cast<byte>(projectionData.data));
     }
-} app;*/
+} app;
 Window window {&layout, str(N)};
