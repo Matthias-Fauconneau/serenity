077fd7a5c87e7854a82c3f10cc4d0030cc55df0c
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2014-07-02 10:41:01 +1000

    Fixes.

--- music/music.cc
+++ music/music.cc
@@ -17,11 +17,21 @@ extern "C" {
 #include <libavutil/avutil.h> //avutil
 }
 
-struct Music /*: Widget*/ {
+#define ENCODE 0
+#define PREVIEW !ENCODE
+#define AUDIO 0
+
+struct Music
+#if PREVIEW
+        : Widget
+#endif
+{
     string name = arguments()[0];
     MusicXML xml = readFile(name+".xml"_);
     Sheet sheet {xml.signs, xml.divisions, 720};
+#if AUDIO || ENCODE
     AudioFile mp3 = name+".mp3"_; // 48KHz AAC would be better
+#endif
     MidiFile midi = readFile(name+".mid"_);
     buffer<uint> noteToBlit = sheet.synchronize(apply(filter(midi.notes, [](MidiNote o){return o.velocity==0;}), [](MidiNote o){return o.key;}));
 
@@ -30,18 +40,23 @@ struct Music /*: Widget*/ {
     uint midiIndex = 0, noteIndex = 0;
     float targetPosition=0, speed=0, position=0; // X target/speed/position of sheet view
 
-    // Encoding
-#define ENCODE 1
+    // Encode
 #if ENCODE
+    bool encode = ENCODE
     Encoder encoder {name, 1280, 720, 60, mp3};
-#else
+#endif
     // Preview
-    bool preview = !encode;
-    Timer timer;
+#if PREVIEW
+    bool preview = PREVIEW;
     Window window {this, int2(1280,720), "MusicXML"_};
+    const uint fps = 60;
+    uint64 videoTime = 0;
+#if AUDIO
     Thread audioThread;
     AudioOutput audio {{this,&Music::read}, audioThread};
-    uint64 audioTime = 0, videoTime = 0;
+    uint64 audioTime = 0;
+#endif
+    bool running = false;
 #endif
     uint noteIndexToMidiIndex(uint seekNoteIndex) {
         uint midiIndex=0;
@@ -52,8 +67,13 @@ struct Music /*: Widget*/ {
     Music() {
 #if PREVIEW
         window.background = White;
-        //seek( midi.notes[noteIndexToMidiIndex(sheet.chordToNote[sheet.measureToChord[100+1]])].time );
-        if(preview) { window.show(); audio.start(mp3.rate, 1024); audioThread.spawn(); }
+        seek( midi.notes[noteIndexToMidiIndex(sheet.chordToNote[sheet.measureToChord[122]])].time );
+        if(preview) {
+            window.show();
+#if AUDIO
+            audio.start(mp3.rate, 1024); audioThread.spawn();
+#endif
+        }
 #else
         /*else*/ {
             Image target(encoder.size());
@@ -75,7 +95,7 @@ struct Music /*: Widget*/ {
                 }
                 while(encoder.audioTime*encoder.fps > encoder.videoTime*encoder.rate) {
                     follow(encoder.videoTime, encoder.fps);
-                    step();
+                    step(1./encoder.fps);
                     renderBackground(target, White);
                     position = min(float(sheet.measures.last()-target.size().x), position);
                     sheet.render(target, int2(floor(-position), 0), target.size());
@@ -108,35 +128,41 @@ struct Music /*: Widget*/ {
         if(active) targetPosition = min(apply(active.values,[this](uint index){return sheet.blits[index].position.x;}));
     }
 
-    void step() {
-        const float dt=1./encoder.fps, k=dt, b=-1; // Time step, stiffness (1/frame) and damping (1) constants
-        speed += dt * (b*speed + k*(targetPosition-position)); // Euler integration of speed (px/frame) from forces of spring equation
-        position += speed; // Euler integration of position (in pixels) from speed (in pixels per frame)
+    void step(const float dt) {
+        const float b=-1, k=1; // damping [1] and stiffness [1/T2] constants
+        speed += dt * (b*speed + k*(targetPosition-position)); // Euler integration of speed (px/s) from acceleration by spring equation (px/s2)
+        position += dt * speed; // Euler integration of position (in pixels) from speed (in pixels/s)
     }
 
 #if PREVIEW
+#if AUDIO
     uint read(const mref<short2>& output) {
         size_t readSize = mp3.read(output);
         audioTime += readSize;
         return readSize;
     }
+#endif
 
     void seek(uint64 midiTime) {
+#if AUDIO
         audioTime = midiTime * mp3.rate / midi.ticksPerSeconds;
         mp3.seek(audioTime); //FIXME: return actual frame time
-        videoTime = audioTime*encoder.fps/mp3.rate; // FIXME: remainder
-        follow(videoTime, 60);
+        videoTime = audioTime*fps/mp3.rate; // FIXME: remainder
+#else
+        videoTime = midiTime * fps / midi.ticksPerSeconds;
+#endif
+        follow(videoTime, fps);
         position = targetPosition;
     }
 
     // Render loop
     void render(const Image& target) override {
-        uint nextTime = window.msc-window.firstMSC;
-        if(videoTime+1 != nextTime) { static int dropCount=0; int drop=nextTime-(videoTime+1); if(drop>0) { dropCount+=drop; log("Dropped",drop,dropCount); } }
-        while(videoTime < nextTime) { follow(videoTime, 60); step(); videoTime++; }
+        follow(videoTime, fps);
+        step(1./fps);
+        videoTime++;
         position = min(float(sheet.measures.last()-target.size().x), position);
         sheet.render(target, int2(floor(-position), 0), target.size());
-        window.render();
+        if(running) window.render();
     }
 #endif
 } app;
