338551efb02eee48742f3c822ae22f893fcab253
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2014-08-19 20:05:30 +0200

    Integrates PDF writer into document compiler.

--- report.cc
+++ report.cc
@@ -6,6 +6,78 @@
 #include "png.h"
 //#include "jpeg.h"
 
+#include "variant.h"
+#include "matrix.h"
+buffer<byte> imagesToPDF(const ref<Image>& images) {
+    array<unique<Object>> objects;
+    auto ref = [&](const Object& object) { return dec(objects.indexOf(&object))+" 0 R"_; };
+
+    objects.append(); // Null object
+
+    Object& root = objects.append();
+    root.insert("Type"_, String("/Catalog"_));
+
+    Object& pages = objects.append();
+    pages.insert("Type"_, String("/Pages"_));
+    pages.insert("Kids"_, array<Variant>());
+    pages.insert("Count"_, 0);
+
+    for(const Image& image: images) {
+        Object& page = objects.append();
+        page.insert("Parent"_, ref(pages));
+        page.insert("Type"_, String("/Page"_));
+        {Dict resources;
+            {Dict xObjects;
+                {Object& xImage = objects.append();
+                    xImage.insert("Subtype"_, String("/Image"_));
+                    xImage.insert("Width"_, dec(image.width));
+                    xImage.insert("Height"_, dec(image.height));
+                    xImage.insert("ColorSpace"_, String("/DeviceRGB"_));
+                    xImage.insert("BitsPerComponent"_, String("8"_));
+                    buffer<byte> rgb3 (image.height * image.width * 3);
+                    assert_(image.buffer.size == image.height * image.width);
+                    for(uint index: range(image.buffer.size)) {
+                        rgb3[index*3+0] = image.buffer[index].r;
+                        rgb3[index*3+1] = image.buffer[index].g;
+                        rgb3[index*3+2] = image.buffer[index].b;
+                    }
+                    xImage = move(rgb3);
+                    xObjects.insert("Image"_, ref(xImage));}
+                resources.insert("XObject"_, move(xObjects));}
+            page.insert("Resources"_, move(resources));}
+        {array<Variant> mediaBox; mediaBox.append( 0 ).append( 0 ).append( image.width ).append( image.height );
+            page.insert("MediaBox"_, move(mediaBox));}
+        {Object& contents = objects.append();
+            mat3 im (vec3(1.f/image.width,0,0), vec3(0,1.f/image.height,0), vec3(0,0,1));
+            mat3 m = im.inverse();
+            contents = dec<float>({m(0,0),m(0,1),m(1,0),m(1,1),m(0,2),m(1,2)})+" cm /Image Do"_;
+            page.insert("Contents"_, ref(contents));}
+        pages.at("Kids"_).list << ref(page);
+        pages.at("Count"_).number++;
+        log(pages.at("Count"_));
+    }
+    root.insert("Pages"_, ref(pages));
+
+    array<byte> file = String("%PDF-1.7\n"_);
+    array<uint> xrefs (objects.size);
+    for(uint index: range(1, objects.size)) {
+        xrefs << file.size;
+        file << dec(index) << " 0 obj\n"_ << str(objects[index]) << "\nendobj\n"_;
+    }
+    int index = file.size;
+    file << "xref\n0 "_ << dec(xrefs.size+1) << "\n0000000000 65535 f\r\n"_;
+    for(uint index: xrefs) {
+        String entry (20);
+        entry << dec(index, 10, '0') << " 00000 n\r\n"_;
+        assert(entry.size==20);
+        file << entry;
+    }
+    Dict trailer; trailer.insert("Size"_, int(1+xrefs.size)); assert_(objects); trailer.insert("Root"_, ref(root));
+    file << "trailer "_ << str(trailer) << "\n"_;
+    file << "startxref\n"_ << dec(index) << "\r\n%%EOF"_;
+    return move(file);
+}
+
 struct Document : Widget {
     static constexpr int2 windowSize = int2(1050, 1485);
     static constexpr int oversample = 1;
@@ -356,22 +428,17 @@ struct Document : Widget {
         return pages;
     }
 
-    void writePages() {
+    array<Image> pages() {
         clear();
-        array<String> args;
-        Folder folder("Rapport.out"_, home(), true);
+        array<Image> images;
         while(s) {
-            Image target (pageSize);
-            layoutPage(target);
-            log(pageIndex);
-            writeFile(dec(pageIndex),encodePNG(target),folder);
-            args << dec(pageIndex);
+            {Image target (pageSize);
+                layoutPage(target);
+                images << move(target);}
             pageIndex++;
+            log(pageIndex);
         }
-        args << String("rapport.pdf"_);
-        log("PDF");
-        execute(which("convert"_),toRefs(args),true,folder);
-        log("Done");
+        return images;
     }
 };
 
@@ -419,7 +486,7 @@ struct Report {
     Report() {
         assert_(arguments());
         /**/ if(arguments()[0]=="export"_) {
-            document.writePages();
+            writeFile("rapport.pdf"_, imagesToPDF(document.pages()));
             exit(0);
             return;
         }
