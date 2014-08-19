5591578d6875f01c95968d4b76fbe563a5d5bcb6
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2014-08-19 14:25:56 +0200

    Restore PDF renderer.

--- test.cc
+++ test.cc
@@ -1,37 +1,15 @@
 #include "widget.h"
 #include "window.h"
 #include "time.h"
+#include "pdf.h"
 
-#if 0
-struct VSyncTest : Widget {
-    Window window{this,int2(512,512),"VSync"_};
-    VSyncTest(){ window.actions[Escape]=[]{exit();}; window.background=NoBackground; window.show(); }
-    uint count=0;
-    Time misc;
-    void render(const Image& target) {
-        misc.start();
-        fill(target, Rect(target.size()), (count++)%2?black:white);
-        window.render();
-        if(count>100) { misc.stop(); exit(); }
+struct PDFTest {
+    /*Scroll<*/PDF/*>*/ pdf;
+    Window window {&pdf/*.area()*/,int2(0,0),"PDFTest"_};
+    PDFTest() {
+        pdf.open(readFile("convert.pdf"_,home()));
+        window.background = White;
+        window.actions[Escape]=[]{exit();};
+        window.show();
     }
-    ~VSyncTest() { assert_(misc); log(count, misc, count/misc.toFloat()); }
 } test;
-#else
-struct Reminder : Widget {
-    const uint64 interval = 60*1000;
-    Timer timer { [this]{ window.show(); } };
-    Window window {this,int2(512,512),"Reminder"_};
-    Reminder() {
-        window.actions[Escape]=[]{exit();}; window.background=White; window.focus=this;
-        timer.setRelative(interval);
-    }
-    void render(const Image&) {}
-    bool keyPress(Key key, Modifiers) {
-        if(key>='1' && key<='9') {
-            timer.setRelative((key-'0')*interval);
-            window.hide();
-        }
-        return false;
-    }
-} app;
-#endif
---
+++ ui/pdf.h
@@ -0,0 +1,93 @@
+#pragma once
+/// \file pdf.h Portable Document Format renderer
+#include "string.h"
+#include "vector.h"
+#include "map.h"
+#include "font.h"
+#include "widget.h"
+#include "function.h"
+#include "time.h"
+
+/// 2D affine transformation (FIXME: use matrix:mat3x2)
+struct mat3x2 {
+    float m11, m12, m21, m22, dx, dy;
+    mat3x2(float m11, float m12, float m21, float m22, float dx, float dy):m11(m11),m12(m12),m21(m21),m22(m22),dx(dx),dy(dy){}
+    mat3x2(float dx, float dy) : mat3x2(1,0,0,1,dx,dy) {}
+    mat3x2() : mat3x2(1,0,0,1,0,0) {}
+    float M(int i, int j) const {assert(i<2 && j<3); return (&m11)[j*2+i]; }
+    float& M(int i, int j) {assert(i<2 && j<3); return (&m11)[j*2+i]; }
+    float operator()(int i, int j) const { return M(i,j); }
+    float& operator()(int i, int j) { return M(i,j); }
+    mat3x2 operator*(mat3x2 m) const { return mat3x2( m11*m.m11 + m12*m.m21, m11*m.m12 + m12*m.m22,
+                                                          m21*m.m11 + m22*m.m21, m21*m.m12 + m22*m.m22,
+                                                          dx*m.m11  + dy*m.m21 + m.dx, dx*m.m12  + dy*m.m22 + m.dy ); }
+    vec2 operator*(vec2 v) const { return vec2( m11*v.x + m21*v.y + dx, m12*v.x + m22*v.y + dy ); }
+};
+
+/// Portable Document Format renderer
+struct PDF : Widget {
+    void clear() { images.clear(); blits.clear(); lines.clear(); fonts.clear(); characters.clear(); paths.clear(); polygons.clear(); annotations.clear(); }
+    void open(const ref<byte>& data);
+    int2 sizeHint() override;
+    void render() override { render(0, target.size()); }
+    void render(int2 offset, int2 size);
+
+    // Current page rendering context
+    mat3x2 Tm,Cm;
+    vec2 boxMin, boxMax;
+    vec2 pageMin, pageMax;
+    void extend(vec2 p) { pageMin=min(pageMin, p), pageMax=max(pageMax, p); }
+
+    // Rendering primitives
+    struct Line { vec2 a,b; bool operator <(const Line& o) const{return a.y<o.a.y || b.y<o.b.y;}};
+    array<Line> lines;
+    enum Flags { Close=1,Stroke=2,Fill=4,OddEven=8,Winding=16,Trace=32 };
+    void drawPath(array<array<vec2>>& paths, int flags);
+
+    struct Fonts {
+        String name;
+        buffer<byte> data;
+        map<float, Font> fonts;
+        array<float> widths;
+    };
+    map<string, Fonts> fonts;
+    Font& getFont(Fonts& fonts, float size);
+
+    struct Character {
+        Fonts* fonts; float size; uint16 index; vec2 position; uint16 code;
+        bool operator <(const Character& o) const{return position.y<o.position.y;}
+    };
+    array<Character> characters;
+    void drawText(Fonts& fonts, int fontSize, float spacing, float wordSpacing, const string& data);
+
+    map<String, Image> images;
+    struct Blit {
+        vec2 position, size; Image image; Image resized;
+        bool operator <(const Blit& o) const{return position.y<o.position.y;}
+    };
+    array<Blit> blits;
+
+    struct Polygon {
+        vec2 min,max; array<Line> edges;
+    };
+    array<Polygon> polygons;
+
+    // Document height (normalized by width=1]
+    float height;
+
+    /// Hooks which can be used to provide additionnal semantics or interactions to the PDF document
+    signal<int /*index*/, vec2 /*position*/, float /*size*/,const string& /*font*/, int /*code*/, int /*fontIndex*/> onGlyph;
+    signal<const ref<vec2>&> onPath;
+    array<array<vec2>> paths;
+
+    map<int,vec3> colors;
+    /// Overrides color for the given characters
+    void setColors(const map<int,vec3>& colors) { this->colors=copy(colors); contentChanged(); }
+    map<vec2, String> annotations;
+    /// Renders additionnal text annotations
+    void setAnnotations(const map<vec2, String>& annotations) { this->annotations=copy(annotations); contentChanged(); }
+
+    signal<> contentChanged;
+    signal<int> hiddenHighlight;
+    int lastSize = 0;
+};
