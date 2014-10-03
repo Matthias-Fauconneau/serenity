f5f996672b3f867d333912a1f8002dbbbb7ea12a
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2014-10-03 11:03:23 +0200

    Restores tuner.

---
+++ tuner/pitch.h
@@ -0,0 +1,68 @@
+#pragma once
+#include "fft.h"
+#include "list.h"
+#include "array.h"
+#include "math.h"
+#include "string.h"
+#include "data.h"
+
+struct PitchEstimator : FFT {
+    using FFT::FFT;
+    // Parameters
+    const uint rate = 96000; // Discards 50Hz harmonics for absolute harmonic energy evaluation
+    const uint fMin = 8, fMax = N/16; // 15 ~ 6000 Hz
+    const uint iterationCount = 4; // Number of least square iterations
+    const float initialInharmonicity = 0; //1./cb(24); // Initial inharmonicity
+    static constexpr float noiseThreshold = 2;
+    static constexpr float highPeakThreshold = 8;
+    const uint medianError = 4;
+    const uint maxHarmonicCount = 16;
+    // Conditions for median F0 override
+    const uint lastHarmonicRank = 39;
+    const uint lastHarmonicFrequency = 868;
+    const uint peakRank = 6;
+    const uint peakFrequency = 113;
+    const uint minNum = 26;
+    const uint minDen = 79;
+    // Conditions for F1 override
+    const uint minHighPeakRank = 27;
+    const uint minHighPeak = 506;
+    const uint minHighPeakNum = 83;
+    const uint minHighPeakDen = 637; // 640
+
+    struct Peak {
+        uint f;
+        bool operator ==(const Peak& o) const { return f == o.f; }
+    };
+    uint minF, maxF;
+    list<Peak, 16> peaks;
+
+    float harmonicEnergy=0;
+    float F0=0, B=0; // F0.n+F0.B.n^2 fit of all harmonics (1st harmonic is F0.(1+B))
+    buffer<float> filteredSpectrum {fMax}; // Filtered power spectrum
+    uint F1, nHigh, medianF0;
+
+    struct Candidate {
+        float f0; float B; float energy; array<uint> peaks;
+        Candidate(float f0=0, float B=0, float energy=0, array<uint>&& peaks={}):
+            f0(f0),B(B),energy(energy),peaks(move(peaks)){}
+    };
+    list<Candidate, 2> candidates;
+
+    /// Returns first partial (f1=f0*sqrt(1+B)~f0*(1+B))
+    /// \a fMin Minimum fundamental frequency for harmonic evaluation
+    float estimate();
+};
+
+inline float keyToPitch(float key) { return 440*exp2((key-69)/12); }
+inline float pitchToKey(float pitch) { return 69+log2(pitch/440)*12; }
+inline String strKey(int key) { return (string[]){"A"_,"A#"_,"B"_,"C"_,"C#"_,"D"_,"D#"_,"E"_,"F"_,"F#"_,"G"_,"G#"_}[(key+2*12+3)%12]+str(key/12-2); }
+inline int parseKey(TextData& s) {
+    int key=24;
+    if(!"cdefgabCDEFGAB"_.contains(s.peek())) return -1;
+    key += "c#d#ef#g#a#b"_.indexOf(toLower(s.next()));
+    if(s.match('#')) key++;
+    key += 12*s.mayInteger(4);
+    return key;
+}
+inline int parseKey(const string& name) { TextData s(name); return parseKey(s); }
---
+++ tuner/profile.h
@@ -0,0 +1,59 @@
+#include "file.h"
+#include "widget.h"
+#include "graphics.h"
+
+const int keyCount = 85;
+float stretch(int m) { return -exp((-54 - m)/12.) + exp((m - 129)/12.); }
+
+struct OffsetPlot : Widget {
+    float offsets[keyCount] = {};
+    float variances[keyCount] = {};
+    OffsetPlot() {
+      if(!existsFile("offsets.profile"_,config())) return;
+      TextData s = readFile("offsets.profile"_,config());
+      for(uint i: range(keyCount)) { offsets[i] = clip(-1./2, s.decimal()/100, 1./2); s.skip(" "_); variances[i] = sq(s.decimal()/100); s.skip("\n"_); }
+    }
+    ~OffsetPlot() {
+        String s;
+        for(uint i: range(keyCount)) s << str(offsets[i]*100) << " "_ << str(sqrt(variances[i])*100) << "\n"_;
+        writeFile("offsets.profile"_, s, config());
+    }
+    void reset() { mref<float>(offsets).clear(); mref<float>(variances).clear(); }
+    int2 sizeHint() { return int2(keyCount*12, -236); }
+    void render(const Image& target) override {
+        int2 size =target.size();
+        float minimumOffset = -1./4;
+        float maximumOffset = 1./4;
+        for(int key: range(keyCount)) {
+            int x0 = key * size.x / keyCount;
+            int x1 = (key+1) * size.x / keyCount;
+
+            float p0 = stretch(key)*12;
+            int y0 = size.y * (maximumOffset-p0) / (maximumOffset-minimumOffset);
+
+            float offset = offsets[key]-p0;
+            float deviation = sqrt(variances[key]);
+            float sign = ::sign(offset) ? : 1;
+
+            // High confidence between zero and max(0, |offset|-deviation)
+            float p1 = max(0.f, abs(offset)-deviation);
+            int y1 = size.y * (maximumOffset-sign*p1-p0) / (maximumOffset-minimumOffset);
+            fill(target, Rect(int2(x0, y0<y1?y0:y1), int2(x1, y0<y1?y1:y0)), sign*p1>0 ? red : blue);
+
+            // Mid confidence between max(0,|offset|-deviation) and |offset|
+            float p2 = abs(offset);
+            int y2 = size.y * (maximumOffset-sign*p2-p0) / (maximumOffset-minimumOffset);
+            fill(target, Rect(int2(x0, y1<y2?y1:y2), int2(x1, y1<y2?y2:y1)), 3.f/4*(sign*p2>0 ? red : blue));
+
+            // Low confidence between |offset| and |offset|+deviation
+            float p3 = abs(offset)+deviation;
+            int y3 = size.y * (maximumOffset-sign*p3-p0) / (maximumOffset-minimumOffset);
+            fill(target, Rect(int2(x0, y2<y3?y2:y3), int2(x1, y2<y3?y3:y2)), 1.f/2*(sign*p3>0 ? red : blue));
+
+            // Low confidence between min(|offset|-deviation, 0) and zero
+            float p4 = min(0.f, abs(offset)-deviation);
+            int y4 = size.y * (maximumOffset-sign*p4-p0) / (maximumOffset-minimumOffset);
+            fill(target, Rect(int2(x0, y0<y4?y0:y4), int2(x1, y0<y4?y4:y0)), 1.f/2*(sign*p4>0 ? red : blue));
+        }
+    }
+};
