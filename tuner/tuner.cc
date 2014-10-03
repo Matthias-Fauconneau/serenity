dc639d51250051e602b28450aecc593f06556091
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2014-10-03 12:59:30 +0200

    Update UI only when needed.

--- tuner/tuner.cc
+++ tuner/tuner.cc
@@ -16,13 +16,14 @@
 /// Estimates fundamental frequency (~pitch) of audio input
 struct Tuner : Poll {
     // Static parameters
+    static constexpr uint rate = 96000;
     static constexpr uint N = 32768; // Analysis window size (A0 (27Hz~4K) * 2 (flat top window) * 2 (periods) * 2 (Nyquist))
-    const uint periodSize = 4096; // Overlaps to increase time resolution (compensates loss from Hann window (which improves frequency resolution))
+    static constexpr uint periodSize = 4096; // Overlaps to increase time resolution (compensates loss from Hann window (which improves frequency resolution))
 
     // Input
-    Thread thread; // Audio thread to buffer periods (when kernel driver buffer was configured too low)
-    AudioInput input{{this,&Tuner::write}, 96000, periodSize, thread};
-    const uint rate = input.rate;
+    Thread thread {-20}; // Audio thread to buffer periods (when kernel driver buffer was configured too low)
+    AudioInput input{{this,&Tuner::write}, rate, periodSize, thread};
+
 
 #if TEST
     const uint lowKey=parseKey(arguments().value(0,"A0"))-12, highKey=parseKey(arguments().value(1,"A7"_))-12;
@@ -50,16 +51,19 @@ struct Tuner : Poll {
     int minWorstKey = 1, maxWorstKey = keyCount;
 
     // UI
-    Text currentKey {""_, 64, white};
-    Text fOffset {""_, 64, white};
-    Text worstKey {""_, 64, white};
-    HBox status {{&currentKey, &fOffset, &worstKey}, HBox::Even};
+    int currentKey = 0;
+    Text currentKeyText {""_, 64, white};
+    int keyOffsetCents = 0;
+    Text keyOffsetText {""_, 64, white};
+    int worstKey = 0;
+    Text worstKeyText {""_, 64, white};
+    HBox status {{&currentKeyText, &keyOffsetText, &worstKeyText}, HBox::Even};
     OffsetPlot profile;
     VBox layout {{&status, &profile}};
     Window window{&layout, int2(1024,600), "Tuner"};
 
     Tuner() {
-        log(__TIME__, input.sampleBits, input.rate, input.periodSize);
+        log(input.sampleBits, input.rate, input.periodSize);
         if(arguments().size>0 && isInteger(arguments()[0])) minWorstKey=fromInteger(arguments()[0]);
         if(arguments().size>1 && isInteger(arguments()[1])) maxWorstKey=fromInteger(arguments()[1]);
 
@@ -81,11 +85,14 @@ struct Tuner : Poll {
         if(audio.read(period) < period.size) { audio.close(); exit(); return; }
         write(period);
         t += period.size;
-        timer.setRelative(period.size*1000/rate); // RT
+        timer.setRelative(period.size*1000/rate); // real time
     }
 #endif
     uint write(const ref<int2>& input) {
-        writeCount.acquire(input.size); // Will overflow if processing thread doesn't follow
+        if(!writeCount.tryAcquire(input.size)) {
+            log("Overflow", writeCount, input.size, readCount);
+            writeCount.acquire(input.size); // Will overflow if processing thread doesn't follow
+        }
         assert(writeIndex+input.size<=signal.size);
         for(uint i: range(input.size)) signal[writeIndex+i] = input[i][0] * 0x1p-24; // Left channel only
         writeIndex = (writeIndex+input.size)%signal.size; // Updates ring buffer pointer
@@ -98,12 +105,18 @@ struct Tuner : Poll {
             readCount.acquire(periodSize); periods++; skipped++;
             readIndex = (readIndex+periodSize)%signal.size; // Updates ring buffer pointer
             writeCount.release(periodSize); // Releases free samples
-            if((float)lastReport > 1) { // Limits report rate
+            /*if((float)lastReport > 1)*/ { // Limits report rate
                 log("Skipped",skipped,"periods, total",periods-frames,"from",periods,"-> Average overlap", 1 - (float) (periods * periodSize) / (frames * N));
                 lastReport.reset(); skipped=0;
+                abort("Unexpected real time processing failure");
             }
         }
-        readCount.acquire(periodSize); periods++;
+        if(!readCount.tryAcquire(periodSize)) {
+            //log("Underflow", readCount, periodSize, writeCount);
+            readCount.acquire(periodSize);
+        }
+        //log("process");
+        periods++;
         frames++;
         assert(readIndex+periodSize<=signal.size);
         for(uint i: range(min<int>(N,signal.size-readIndex))) estimator.windowed[i] = estimator.window[i] * signal[readIndex+i];
@@ -128,11 +141,20 @@ struct Tuner : Poll {
         }
         if(confidence > 1./confidenceThreshold/2 && 1-ambiguity > 1./ambiguityThreshold/2 && confidence*(1-ambiguity) > 1./threshold/2
                 && abs(offset)<offsetThreshold ) {
-
-            currentKey = Text(strKey(key), 64, white);
+            bool needUpdate = false;
+            if(currentKey!=key) {
+                currentKey = key;
+                currentKeyText = Text(strKey(key), 64, white);
+                needUpdate = true;
+            }
             if(key!=lastKey) keyOffset = offset; // Resets on key change
             keyOffset = (keyOffset+offset)/2; // Running average
-            fOffset = Text(dec(round(100*keyOffset)), 64, white);
+            /*int keyOffsetCents = round(100*keyOffset);
+            if(this->keyOffsetCents != keyOffsetCents) {
+                this->keyOffsetCents = keyOffsetCents;
+                keyOffsetText = Text(dec(keyOffsetCents), 64, white);
+                needUpdate = true;
+            }*/
 
             if(record && confidence >= 1./confidenceThreshold && 1-ambiguity > 1./ambiguityThreshold && confidence*(1-ambiguity) > 1./threshold
                     && key>=21 && key<21+keyCount) {
@@ -141,13 +163,22 @@ struct Tuner : Poll {
                 float& keyVariance = profile.variances[key-21]; keyVariance = (1-alpha)*keyVariance + alpha*sq(offset - keyOffset);
                 int k = -1;
                 for(uint i: range(minWorstKey, maxWorstKey)) if(k<0 || abs(profile.offsets[i] - stretch(i)*12) > abs(profile.offsets[k] - stretch(k)*12)) k = i;
-                worstKey = Text(strKey(21+k), 64, white);
-                fOffset = Text(dec(round(100*keyOffset)), 64, white);
+                int worstKey = 21+k;
+                if(this->worstKey != worstKey) {
+                    this->worstKey = worstKey;
+                    worstKeyText = Text(strKey(worstKey), 64, white);
+                }
+                int keyOffsetCents = round(100*keyOffset);
+                if(this->keyOffsetCents != keyOffsetCents) {
+                    this->keyOffsetCents = keyOffsetCents;
+                    keyOffsetText = Text(dec(keyOffsetCents), 64, white);
+                    needUpdate = true;
+                }
             }
+            if(needUpdate) window.render();
         }
 
         readIndex = (readIndex+periodSize)%signal.size; // Updates ring buffer pointer
         writeCount.release(periodSize); // Releases free samples (only after having possibly recorded the whole ring buffer)
-        window.render();
     }
 } app;
