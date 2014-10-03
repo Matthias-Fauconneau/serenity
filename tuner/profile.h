f61c16cc47c27b8a6c57c0f85b2b38b68e6523f5
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2014-10-03 17:29:43 +0200

    Correct profile drawing.

--- tuner/profile.h
+++ tuner/profile.h
@@ -27,6 +27,7 @@ struct OffsetPlot : Widget {
         for(int key: range(keyCount)) {
             int x0 = key * size.x / keyCount;
             int x1 = (key+1) * size.x / keyCount;
+            int dx = x1-x0;
 
             float p0 = stretch(key) * 12;
             int y0 = size.y * (maximumOffset-p0) / (maximumOffset-minimumOffset);
@@ -38,22 +39,22 @@ struct OffsetPlot : Widget {
             // High confidence between zero and max(0, |offset|-deviation)
             float p1 = max(0.f, abs(offset)-deviation);
             int y1 = size.y * (maximumOffset-sign*p1-p0) / (maximumOffset-minimumOffset);
-            graphics.fills << Fill{vec2(x0, y0<y1?y0:y1), vec2(x1, y0<y1?y1:y0), sign*p1>0 ? red : blue, 1};
+            graphics.fills << Fill{vec2(x0, min(y0,y1)), vec2(dx, abs(y0-y1)), sign*p1>0 ? red : blue, 1};
 
             // Mid confidence between max(0,|offset|-deviation) and |offset|
             float p2 = abs(offset);
             int y2 = size.y * (maximumOffset-sign*p2-p0) / (maximumOffset-minimumOffset);
-            graphics.fills << Fill{vec2(x0, y1<y2?y1:y2), vec2(x1, y1<y2?y2:y1), 3.f/4*(sign*p2>0 ? red : blue), 1};
+            graphics.fills << Fill{vec2(x0, min(y1,y2)), vec2(dx, abs(y1-y2)), 3.f/4*(sign*p2>0 ? red : blue), 1};
 
             // Low confidence between |offset| and |offset|+deviation
             float p3 = abs(offset)+deviation;
             int y3 = size.y * (maximumOffset-sign*p3-p0) / (maximumOffset-minimumOffset);
-            graphics.fills << Fill{vec2(x0, y2<y3?y2:y3), vec2(x1, y2<y3?y3:y2), 1.f/2*(sign*p3>0 ? red : blue), 1};
+            graphics.fills << Fill{vec2(x0, min(y2,y3)), vec2(dx, abs(y2-y3)), 1.f/2*(sign*p3>0 ? red : blue), 1};
 
             // Low confidence between min(|offset|-deviation, 0) and zero
             float p4 = min(0.f, abs(offset)-deviation);
             int y4 = size.y * (maximumOffset-sign*p4-p0) / (maximumOffset-minimumOffset);
-            graphics.fills << Fill{vec2(x0, y0<y4?y0:y4), vec2(x1, y0<y4?y4:y0), 1.f/2*(sign*p4>0 ? red : blue), 1};
+            graphics.fills << Fill{vec2(x0, min(y4,y0)), vec2(dx, abs(y4-y0)), 1.f/2*(sign*p4>0 ? red : blue), 1};
         }
         return graphics;
     }
