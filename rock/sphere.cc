aa0f2725548c6b56ea92275d927750c6564eb5bf
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2014-10-16 18:42:50 +0200

    Renames equivalent volume and variance radius. Adds maximum radius.

--- rock/sphere.cc
+++ rock/sphere.cc
@@ -1,10 +1,10 @@
 /// \file sphere.cc Computes radius for each list of voxels
-#include "operation.h"
+#include "volume-operation.h"
 #include "list.h"
 #include "sample.h"
 
 /// Computes radius for each list of voxels as the radius of the ball with the same variance
-array<real> radiusList(const buffer<array<short3>>& lists) {
+array<real> varianceRadius(const buffer<array<short3>>& lists) {
     array<real> radiusList (lists.size);
     for(ref<short3> list: lists) {
         if(!list) continue;
@@ -19,28 +19,49 @@ array<real> radiusList(const buffer<array<short3>>& lists) {
     return move(radiusList);
 }
 
-/// Computes radius for each list of voxels
-struct RadiusList : Operation {
+/// Computes equivalent variance radius for each list of voxels
+struct VarianceRadius : Operation {
     virtual void execute(const Dict&, const ref<Result*>& outputs, const ref<const Result*>& inputs) override {
-        array<real> radiusList = ::radiusList(parseLists(inputs[0]->data));
+        array<real> radiusList = ::varianceRadius(parseLists(inputs[0]->data));
         outputs[0]->metadata = String("r(index).tsv"_);
         outputs[0]->data = toASCII(UniformSample(radiusList));
     }
 };
-template struct Interface<Operation>::Factory<RadiusList>;
+template struct Interface<Operation>::Factory<VarianceRadius>;
 
 
 /// Computes an equivalent ball radius from volume
-array<real> equivalentRadius(const buffer<real>& volume) {
+array<real> volumeRadius(const buffer<real>& volume) {
     return apply(volume, [](real V){ return cbrt(3/(4*PI) * V); }); // V = 4π/3 r³ <=> r = (3/4π V) ¹/³
 }
 
-/// Computes radius for each list of voxels
-struct EquivalentRadius : Operation {
+/// Computes equivalent volume radius for each list of voxels
+struct VolumeRadius : Operation {
     virtual void execute(const Dict&, const ref<Result*>& outputs, const ref<const Result*>& inputs) override {
-        array<real> radiusList = ::equivalentRadius(parseUniformSample(inputs[0]->data));
+        array<real> radiusList = ::volumeRadius(parseUniformSample(inputs[0]->data));
         outputs[0]->metadata = String("r(index).tsv"_);
         outputs[0]->data = toASCII(UniformSample(radiusList));
     }
 };
-template struct Interface<Operation>::Factory<EquivalentRadius>;
+template struct Interface<Operation>::Factory<VolumeRadius>;
+
+/// Computes maximum radius for each list of voxels
+array<real> maximumRadius(const VolumeT<uint16>& maximum, const buffer<array<short3>>& lists) {
+    array<real> radiusList (lists.size);
+    for(ref<short3> list: lists) {
+        if(!list) continue;
+        radiusList << sqrt((real) max(apply(list, [&](short3 p){ return maximum(p.x, p.y, p.z); })));
+    }
+    return move(radiusList);
+}
+
+/// Computes maximum radius for each list of voxels
+struct MaximumRadius : Operation {
+    virtual void execute(const Dict&, const ref<Result*>& outputs, const ref<const Result*>& inputs) override {
+        Volume source = toVolume(*inputs[0]);
+        array<real> radiusList = maximumRadius(source, parseLists(inputs[1]->data));
+        outputs[0]->metadata = String("r(index).tsv"_);
+        outputs[0]->data = toASCII(UniformSample(radiusList));
+    }
+};
+template struct Interface<Operation>::Factory<MaximumRadius>;
