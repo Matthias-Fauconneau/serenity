020c6d32948ee33f16db2ff0c73e7d8788093ffd
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2014-12-01 10:00:38 +0100

    MusicXML to PDF page stub.

---
+++ music-pdf.cc
@@ -0,0 +1,22 @@
+#include "MusicXML.h"
+#include "sheet.h"
+#include "window.h"
+#include "interface.h"
+
+struct MusicPDF {
+	// Name
+	string name = arguments() ? arguments()[0] : (error("Expected name"), string());
+
+	// MusicXML
+	MusicXML xml = readFile(name+".xml"_);
+
+	// Sheet
+	Sheet sheet {xml.signs, xml.divisions};
+
+	// Page preview
+	static constexpr float inchMM = 25.4, inchPx = 90;
+	const int2 pageSize = int2(210/*mm*/ * (inchPx/inchMM), 297/*mm*/ * (inchPx/inchMM));
+	Window window {&sheet, pageSize, [](){return "MusicPDF"__;}};
+
+	MusicPDF() { window.background = Window::White; window.show(); }
+} app;
