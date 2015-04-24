5514d0c2e51a88572be9c5ef53c5bfb6cf483375
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2015-04-24 13:36:31 +0200

    Rotation.

--- DEM.cc
+++ DEM.cc
@@ -1,9 +1,17 @@
 #include "thread.h"
 #include "window.h"
 #include "time.h"
+//include "matrix.h"
 
 // -> vector.h
 template<template<Type> /*Type*/class V, Type T, uint N> inline /*constexpr*/ float length(const vec<V,T,N>& a) { return sqrt(dot(a,a)); }
+
+struct quat {
+    float s; vec3 v;
+    quat conjugate() { return {s, -v}; }
+};
+quat operator*(quat p, quat q) { return {p.s*q.s - dot(p.v, q.v), p.s*q.v + q.s*p.v + cross(p.v, q.v)}; }
+String str(quat q) { return "["+str(q.s, q.v)+"]"; }
 
 struct DEM : Widget, Poll {
     Random random;
@@ -13,11 +21,17 @@ struct DEM : Widget, Poll {
     const float floorNormalDamping = 10, floorTangentialDamping = 10;
 
     struct Particle {
-        float radius;
-        vec3 position, velocity;
+        const float radius;
+        const float mass;
+        vec3 position;
+        vec3 velocity = 0;
+        quat rotation {1, 0};
+        vec3 angularVelocity = 0; // Global
+        Particle(vec3 position, float radius=1./16)
+            : radius(radius), mass(1/**4./3*PI*cb(radius)*/), position(position) {}
     };
     array<Particle> particles;
-    const float particleYoung = 10000, particlePoisson = 0;
+    const float particleYoung = 1000, particlePoisson = 0;
     const float particleNormalDamping = 10, particleTangentialDamping = 1;
 
     void event() { step(); }
@@ -26,25 +40,22 @@ struct DEM : Widget, Poll {
         for(auto& p: particles) {
             if(p.position.y - p.radius < -1) generate = false;
         }
-        if(generate) {
-            particles.append({1./16,vec3(random()*2-1,-1,0),0});
-        }
+        if(generate && particles.size<1024) particles.append(vec3(random()*2-1,-1,0));
         for(auto& p: particles) {
-            vec3 force = 0, torque = 0;
+            vec3 force = 0;
+            vec3 torque = 0; // Global
             // Gravity
-            const float g = 1;
-            const float mass = 1;
-            force.y += g * mass;
+            const vec3 g (0.0, 0.1, 0);
+            force += p.mass * g;
             // Elastic sphere - half space contact
             float d = (p.position.y + p.radius) - floorHeight;
             if(d > 0) {
                 float E = 1/( (1-sq(floorPoisson))/floorYoung + (1-sq(particlePoisson))/particleYoung );
                 float kN = floorNormalDamping; // 1/(1/floorDamping + 1/particleDamping)) ?
-                vec3 v = p.velocity;
                 vec3 n (0, -1, 0);
+                vec3 v = p.velocity + cross(p.angularVelocity, vec3(0, p.radius, 0));
                 float vN = dot(n, v);
                 float fN = 4/3*E*sqrt(p.radius)*d*sqrt(d) - kN * vN;
-                //assert(fN >= 0, fN);
                 force += fN * n;
                 vec3 vT = v - dot(n, v)*n;
                 float friction = -1; // mu
@@ -52,8 +63,9 @@ struct DEM : Widget, Poll {
                 const float smoothCoulomb = 0.1;
                 float sC = 1- smoothCoulomb/(smoothCoulomb+sq(vT));
                 if(length(vT)) {
-                    float fT = friction * abs(fN) * sC / length(vT) - kT;
-                    force += fT * vT;
+                    vec3 fT = (friction * abs(fN) * sC / length(vT) - kT) * vT;
+                    force += fT;
+                    torque += cross(p.radius*(-n), fT);
                 }
                 assert(isNumber(force));
             }
@@ -66,32 +78,44 @@ struct DEM : Widget, Poll {
                     float E = 1/((1-sq(particlePoisson))/particleYoung + (1-sq(particlePoisson))/particleYoung);
                     float kN = particleNormalDamping;
                     float R = 1/(1/p.radius+1/b.radius);
-                    vec3 v = p.velocity-b.velocity;
+                    vec3 v = p.velocity + cross(p.angularVelocity, vec3(0, p.radius, 0))
+                           -(b.velocity + cross(b.angularVelocity, vec3(0, b.radius, 0)));
                     vec3 n = r/length(r);
                     float fN = 4/3*E*sqrt(R)*sqrt(d)*d - kN * dot(n, v);
                     force += fN * n;
-                    /*float friction = 0; // mu
-                    float kT = 0;//particleDamping;
-                    vec2 t = vec2(n.y, -n.x); //cross(n);
-                    float fT = friction * fN - kT * dot(t, p.velocity-b.velocity);
-                    force += fT * t;*/
                     vec3 vT = v - dot(n, v)*n;
                     float friction = -1; // mu
                     float kT = particleTangentialDamping;
                     const float smoothCoulomb = 0.1;
                     float sC = 1- smoothCoulomb/(smoothCoulomb+sq(vT));
                     if(length(vT)) {
-                        float fT = friction * abs(fN) * sC / length(vT) - kT;
-                        force += fT * vT;
+                        vec3 fT = (friction * abs(fN) * sC / length(vT) - kT) * vT;
+                        force += fT;
+                        torque += cross(p.radius*(-n), fT);
                     }
                     assert(isNumber(force));
                 }
             }
-            // Euler explicit integration
+            // Euler position integration (TODO: Velocity verlet)
             const float dt = 1./60;
-            p.velocity += dt * force / mass;
+            p.velocity += dt * force / p.mass;
             p.position += dt * p.velocity;
             assert(isNumber(p.position), p.position, p.velocity, force);
+            // PCDM rotation integration
+            /*mat3*/ float I (2./3*p.mass*sq(p.radius));
+            vec3 w = (p.rotation.conjugate() * quat{0, p.angularVelocity} * p.rotation).v; // Local
+            vec3 t = (p.rotation.conjugate() * quat{0, torque} * p.rotation).v; // Local
+            vec3 dw = 1/I * (t - cross(w, I*w));
+            vec3 w4 = w + dt/4*dw;
+            float w4l = length(w4);
+            // Prediction (multiplicative update)
+            quat qp = w4l ? quat{cos(dt/4*w4l), sin(dt/4*w4l)*w4/w4l} * p.rotation : p.rotation;
+            vec3 w2 = w + dt/2*dw;
+            vec3 w2w = (qp * quat{0, w2} * qp.conjugate()).v; // Global
+            float w2wl = length(w2w);
+            // Correction (multiplicative update)
+            p.rotation = w2wl ? quat{cos(dt/2*w2wl), sin(dt/2*w2wl)*w2w/length(w2w)} * p.rotation : p.rotation;
+            p.angularVelocity = (p.rotation * quat{0, w + dt*dw} * p.rotation.conjugate()).v; // Global
         }
 
         window->render();
@@ -119,6 +143,10 @@ struct DEM : Widget, Poll {
                 vec2 B = scale * (p.position.xy() + p.radius*vec2(cos(b),sin(b))) + offset;
                 graphics->lines.append(A, B);
             }
+            vec3 al = p.radius*vec3(1,0,0);
+            vec3 aw = p.position + (p.rotation * quat{0, al} * p.rotation.conjugate()).v;
+            assert_(isNumber(aw), aw, p.position, p.rotation, al);
+            graphics->lines.append(scale*p.position.xy()+offset, scale*aw.xy()+offset);
         }
         return graphics;
     }
