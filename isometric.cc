8330771b7a8adbe6077cce3eb69bd82856bb9cae
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2015-05-25 15:06:13 +0200

    Isometric test.

--- isometric.cc
+++ isometric.cc
@@ -6,27 +6,32 @@ struct Isometric : Widget {
     unique<Window> window = ::window(this, 0);
     vec2 sizeHint(vec2) { return vec2(1366, 720); }
     shared<Graphics> graphics(vec2 unused size) {
-        Image target = Image(int2(size));
-        target.clear(0);
-        const int n = 16, N = 32;
-        /*for(int y: range(size.y/S)) {
-            for(int x: range(size.x/S)) {
-                target(x*S,y*S) = 0;
-            }
-        }*/
+        const int N = 512;
+        const int W=size.x, H=size.y;
+        buffer<float3> samples (H*W*4); // 4 samples / pixel
         for(int y: range(-N,N)) {
             for(int x: range(-N,N)) {
-                int Z = int(sin(2*PI*float(x)/N)*sin(2*PI*float(y)/N)*4);
-                for(int z: range(Z-1, Z+1)) {
-                    int X = x + y;
-                    int Y = x - y - z*2;
-                    int ix = (size.x/n)/2 + X/2, fx = X%2;
-                    int iy = (size.y/n)/2 + Y/4, fy = Y%4;
-                    uint px = ix*n+fx*n/2;
-                    uint py = iy*n+fy*n/4;
-                    if(px < uint(target.size.x) && py < uint(target.size.y)) target(px, py) = byte4((4+z)*0xFF/8, fx*0xFF/2, fy*0xFF/4, 0xFF);
+                const float A = 256;
+                int Z = int(sin(PI*float(x)/N)*sin(PI*float(y)/N)*A);
+                for(int z: range(Z-2, Z+1)) {
+                    int X = W + x + y;
+                    int Y = H*2 - x + y - z*2;
+                    assert_(X>=0 && Y>=0, x, y, z, X, Y);
+                    int ix = X/2, fx = X%2;
+                    int iy = Y/4, fy = Y%4;
+                    samples[(iy*W+ix)*4+fy/2*2+fx] = float3(max(0.f, (A+z)/(A*2)));
                 }
             }
+        }
+        // Resolve
+        Image target = Image(int2(size));
+        for(size_t i: range(H*W)) {
+            float3 sum = 0;
+            for(float3 sample: samples.slice(i*4, 4)) sum += sample;
+            sum /= 4;
+            int3 linear (round(float(0xFFF)*sum));
+            extern const uint8 sRGB_forward[0x1000];
+            target[i] = byte4(sRGB_forward[linear[0]], sRGB_forward[linear[1]], sRGB_forward[linear[2]], 0xFF);
         }
         shared<Graphics> graphics;
         graphics->blits.append(0, size, move(target));
