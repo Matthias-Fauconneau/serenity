2c47d2bac9f7536792a9e41cc76285b236b6eabd
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2015-05-26 23:16:36 +0200

    Match imagery with LIDAR.

--- isometric.cc
+++ isometric.cc
@@ -9,7 +9,7 @@ struct Isometric : Widget {
         byte3 color;
     };
     buffer<Point> points;
-    short4 min = -1, mean, max = 0;
+    short4 min = 0x7FFF, mean, max = 0;
     Isometric() {
         Map map ("6805_2520.las"_, home());
         struct Header {
@@ -51,15 +51,16 @@ struct Isometric : Widget {
         } packed;
         assert_(sizeof(LASPoint)==header.stride, sizeof(LASPoint), header.stride);
         points = buffer<Point>(header.count, 0);
-        log(minD, maxD);
+        //log(minD, maxD);
         int3 minI ((minD-header.offset)/header.scale), maxI ((maxD-header.offset)/header.scale);
         //int scale = (maxI.x-minI.x) / 2048;
         long4 sum;
         enum { Unclassified, Undefined, Ground, LowVegetation, Vegetation, HighVegetation, Building, Noise, Model, Water, Reserved1, Reserved2, Overlap };
         Image image = decodeJPEG(Map("6805_2520.jpg"_, home()));
+        const int N = 1024;
         for(LASPoint p: cast<LASPoint>(map.slice(header.data)).slice(0, header.count)) {
             if(p.classification == Noise || p.classification == Overlap) continue;
-            short3 position((p.position-minI)*2047/(maxI.x-minI.x));
+            short3 position((p.position-minI)*(N-1)/(maxI.x-minI.x));
             short4 point(position, ::min(0xFF, int(p.intensity)));
             min = ::min(min, point);
             max = ::max(max, point);
@@ -72,28 +73,29 @@ struct Isometric : Widget {
             else if(p.classification == Building) colorIndex = 4;
             else colorIndex = 5; //error(withName(p.classification));*/
             assert_(position.x >= 0 && position.x < 2048 && position.y >= 0 && position.y < 2048, position);
-            byte4 color = image(position.y*2, 4095-position.x*2);
+            byte4 color = image(position.x*(4096-1)/(N-1), 4096-1-position.y*(4096-1)/(N-1));
             points.append({position, byte3(color.b, color.g, color.r)});
         }
         mean = short4(sum / int64(points.size));
         for(Point& p: points) p.position -= short3(mean.x, mean.y, mean.z);
-        //log(min, mean, max, header.count, log2(float(header.count)), points.size, log2(float(points.size)));
+        log(min, mean, max, header.count, log2(float(header.count)), points.size, log2(float(points.size)));
     }
 
     vec2 sizeHint(vec2) { return vec2(0); }
     float pitch = PI/6, yaw = PI/2;
+    //float pitch = PI/2, yaw = 0;
     shared<Graphics> graphics(vec2 unused size) {
         const int W=size.x, H=size.y;
         Image target = Image(int2(size));
         target.clear(0);
         float Mxx = cos(yaw), Mxy = -sin(yaw);
         float Myx = -sin(pitch)*sin(yaw), Myy = -sin(pitch)*cos(yaw), Myz = -cos(pitch);
-        //byte4 colors[] = {byte4(0x50,0x60,0x80), byte4(0,0x40,0), byte4(0,0x80,0), byte4(0,0xFF,0), byte4(0x40,0x40,0xA0), 0xFF};
         for(Point p: points) {
             int x = p.position.x, y = p.position.y, z = p.position.z;
             int X = W/2 + Mxx * x + Mxy * y;
             int Y = H/2 + Myx * x + Myy * y + Myz * z;
-            if(X >= 0 && Y>= 0 && Y < H && X < W) target(X, Y) = p.color;
+            //int z8 = 0xFF*((mean.z+z)-min.z)/(max.z-min.z);
+            if(X >= 0 && Y>= 0 && Y < H && X < W) /*if(z8>target(X, Y)[0])*/ target(X, Y) = p.color;
         }
         shared<Graphics> graphics;
         graphics->blits.append(0, size, move(target));
