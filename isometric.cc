2d66a1b4c26870cd6f983a6425161d99c4559ff9
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2015-05-27 21:59:28 +0200

    Clips anormal columns.

--- isometric.cc
+++ isometric.cc
@@ -36,7 +36,7 @@ struct Isometric : Widget {
         } packed;
         double3 minD (header.minX, header.minY, header.minZ), maxD (header.maxX, header.maxY, header.maxZ);
         int3 minI ((minD-header.offset)/header.scale), maxI ((maxD-header.offset)/header.scale);
-        buffer<int> histogram (maxI.z-minI.z);
+        buffer<int> histogram ((maxI.z-minI.z)*int(N-1)/(maxI.x-minI.x));
         histogram.clear(0);
         for(LASPoint point: cast<LASPoint>(map.slice(header.data)).slice(0, header.count)) {
             if(point.classification <= 1) continue;
@@ -46,21 +46,42 @@ struct Isometric : Widget {
             extent[1] = max(extent[1], p.z);
             histogram[p.z]++;
         }
-        int clipZ = 0;
-        for(size_t z: range(histogram.size)) {
-            if(histogram[z] > histogram[clipZ]) {
-                clipZ = z;
+        int16 minClipZ = 0;
+        for(int z: range(histogram.size)) {
+            if(histogram[z] > histogram[minClipZ]) {
+                minClipZ = z;
                 int dz = 1;
                 for(;z+dz<histogram.size;dz++) if(histogram[z+dz] < histogram[z+dz-1]) break;
                 dz--;
-                if(dz >= 7) break;
+                if(dz > 5) break;
             }
         }
-        log(clipZ, histogram[clipZ], histogram.size);
+        int16 maxClipZ = histogram.size-1;
+        for(int z: reverse_range(histogram.size)) {
+            if(histogram[z] > histogram[maxClipZ]) {
+                maxClipZ = z;
+                int dz = 1;
+                for(;z-dz>=0;dz++) if(histogram[z-dz] < histogram[z-(dz-1)]) break;
+                dz--;
+                if(dz > 23) break;
+            }
+        }
         size_t voxelCount = 0;
-        for(auto& extent: extents) {
-            extent[0] = max<int>(clipZ, extent[0]);
+        int16 minZ = 0x7FFF;
+        for(int y: range(extents.size.y)) for(int x: range(extents.size.x)) {
+            auto& extent = extents(x, y);
+            {int16 neighbourMinZ = 0x7FFF;
+            for(int Y: range(max(0, y-1), min(N, y+1+1))) for(int X: range(max(0, x-1), min(N, x+1+1)))
+                if(Y != y || X != x) neighbourMinZ = min(neighbourMinZ, extents(X,Y)[0]);
+            extent[0] = max(max(minClipZ, extent[0]), neighbourMinZ);}
+
+            {int16 neighbourMaxZ = 0;
+            for(int Y: range(max(0, y-1), min(N, y+1+1))) for(int X: range(max(0, x-1), min(N, x+1+1)))
+                if(Y != y || X != x) neighbourMaxZ = max(neighbourMaxZ, extents(X,Y)[1]);
+            extent[1] = min(min(maxClipZ, extent[1]), neighbourMaxZ);}
+
             if(extent[0] > extent[1]) { extent[1]=extent[0]-1; continue; }
+            minZ = min(minZ, extent[0]);
             voxelCount += extent[1]+1-extent[0];
         }
         //log(columnCount, maxHeight, voxelCount, voxelCount/columnCount);
@@ -70,7 +91,8 @@ struct Isometric : Widget {
         for(int y: range(extents.size.y)) for(int x: range(extents.size.x)) {
             byte3 color = image(x*4, 4096-1-y*4).bgr();
             auto& extent = extents(x, y);
-            for(int z = extent[0]; z<=extent[1]; z++, voxelIndex++) voxels[voxelIndex] = color;
+            extent[0] -= minZ, extent[1] -= minZ;
+            for(int z = extent[0]; z<=extent[1]; z++, voxelIndex++) voxels[voxelIndex] = color; // TODO: detail (from intensity), allow concave
         }
     }
 
@@ -82,13 +104,17 @@ struct Isometric : Widget {
         float Mxx = cos(yaw), Mxy = -sin(yaw);
         float Myx = -sin(pitch)*sin(yaw), Myy = -sin(pitch)*cos(yaw), Myz = -cos(pitch);
         size_t voxelIndex = 0;
+        Image16 zBuffer (W, H); // implicit voxels indexing prevents alternative traversal orders
+        zBuffer.clear(0);
         for(int y: range(extents.size.y)) for(int x: range(extents.size.x)) {
+            //if(Myx > 0) x = N-1-x;
+            //if(Myy > 0) y = N-1-y;
             auto& extent = extents(x, y);
             int X = W/2 + Mxx * (x-N/2) + Mxy * (y-N/2);
             float Y0 = H/2 + Myx * (x-N/2) + Myy * (y-N/2);
             for(int z = extent[0]; z<=extent[1]; z++, voxelIndex++) {
                 int Y = Y0 + Myz * z;
-                if(X >= 0 && Y>= 0 && Y < H && X < W) target(X, Y) = voxels[voxelIndex];
+                if(X >= 0 && Y>= 0 && Y < H && X < W && z > zBuffer(X, Y)) { zBuffer(X,Y) = z; target(X, Y) = voxels[voxelIndex]; }
             }
         }
         shared<Graphics> graphics;
