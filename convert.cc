a881f499a979c976c26817f2e441f3db2f2caee6
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2016-07-28 11:42:46 +0000

    Y median.

--- convert.cc
+++ convert.cc
@@ -11,19 +11,11 @@ struct Convert {
   const Image16& image = cr2.image;
   constexpr size_t cropY = 18, cropX = 96;
   uint min = -1, max = 0;
-  buffer<uint32> histogram (1<<13); // 256K
-  histogram.clear(0);
   for(size_t y: range(cropY, image.height-cropY)) for(size_t x: range(cropX, image.width-cropX)) {
    const uint v = image(x,y);
-   assert_(v < 1<<13, v);
    min = ::min<uint>(min, v);
    max = ::max<uint>(max, v);
-   histogram[v]++;
   }
-  uint bin = 0; for(uint count =0; count < size_t((image.height-2*cropY)*(image.width-2*cropX)/2); bin++) count += histogram[bin];
-  assert_(bin > min);
-  bin -= min;
-  log(bin);
 
   mat3 camXYZ {
     vec3( 0.9602, -0.2984, -0.0407),
@@ -34,57 +26,50 @@ struct Convert {
     vec3(0.3576, 0.7152, 0.1192),
     vec3(0.1805, 0.0722, 0.9505) };
   mat3 camRGB = camXYZ*XYZrgb;
-  if(1) for(int i: range(3)) { // Normalizes so that camRGB * 1 is 1
+  for(int i: range(3)) { // Normalizes so that camRGB * 1 is 1
    float sum = 0;
    for(int j: range(3)) sum += camRGB(i, j);
+   log(sum);
    for(int j: range(3)) camRGB(i, j) /= sum;
   }
-  //uint maxBalance = ::max(::max(cr2.whiteBalance.R, cr2.whiteBalance.G), cr2.whiteBalance.B);
-  //vec3 whiteBalance ( (float)cr2.whiteBalance.R/maxBalance, (float)cr2.whiteBalance.G/maxBalance/2, (float)cr2.whiteBalance.B/maxBalance );
-  uint minBalance = ::min(::min(cr2.whiteBalance.R, cr2.whiteBalance.G), cr2.whiteBalance.B);
-  vec3 whiteBalance ( (float)cr2.whiteBalance.R/minBalance, (float)cr2.whiteBalance.G/minBalance/2, (float)cr2.whiteBalance.B/minBalance );
-  //mat3 m = camRGB.inverse() * mat3(whiteBalance*(((1<<15)-1)/float(int(max-min))));
-  log(bin, log2(bin/float(1<<14)), float(1<<10)/bin, log2(float(1<<10)/bin));
-  mat3 m = camRGB.inverse() * mat3(whiteBalance*(((1<<(14-4))-1)/float(bin)));
+  uint maxBalance = ::min(::max(cr2.whiteBalance.R, cr2.whiteBalance.G), cr2.whiteBalance.B);
+  vec3 whiteBalance ( (float)cr2.whiteBalance.R/maxBalance, (float)cr2.whiteBalance.G/maxBalance/2, (float)cr2.whiteBalance.B/maxBalance );
+  mat3 m = camRGB.inverse() * mat3(whiteBalance*(((1<<15)-1)/float(int(max-min))));
   constexpr int shift = 31-15-7; float scale = 1<<shift;
   for(float c: m.data) assert_(c < 1<<7);
-  int32 m00 = scale*m(0,0), m01 = scale*m(0, 1), m02 = scale*m(0, 2);
-  int32 m10 = scale*m(1,0), m11 = scale*m(1, 1), m12 = scale*m(1, 2);
-  int32 m20 = scale*m(2,0), m21 = scale*m(2, 1), m22 = scale*m(2, 2);
-
+  int32 m00 = scale*m(0,0), m01 = scale*m(0, 1), m02 = scale*m(0, 2); // R
+  int32 m10 = scale*m(1,0), m11 = scale*m(1, 1), m12 = scale*m(1, 2); // G
+  int32 m20 = scale*m(2,0), m21 = scale*m(2, 1), m22 = scale*m(2, 2); // B
+  int32 m30 = XYZrgb(1, 0)*(m00+m10+m20), m31 = XYZrgb(1, 0)*(m01+m11+m21), m32 = XYZrgb(1, 0)*(m02+m12+m22); // Y
+  buffer<uint32> histogram (1<<15); // 128K
+  histogram.clear(0);
   int2 size ((image.width-cropX)/2, (image.height-cropY)/2);
   Image16 R(size), G(size), B(size);
-  /*buffer<uint32> histogram (1<<16); // 256K
-  histogram.clear(0);*/
-  for(size_t Y: range(size.y)) {
-   for(size_t X: range(size.x)) {
-    size_t y = cropY+2*Y, x = cropX+2*X;
+  for(size_t tY: range(size.y)) {
+   for(size_t tX: range(size.x)) {
+    size_t y = cropY+2*tY, x = cropX+2*tX;
     int32 cR = image(x+0,y+0)-min;
     int32 cG = image(x+1,y+0)+image(x+0,y+1)-2*min;
     int32 cB = image(x+1,y+1)-min;
-    //histogram[::max(0, cR+cG+cB)/3]++; // FIXME: brightness?
     int r = (m00 * cR + m01 * cG + m02 * cB)>>shift;
     int g = (m10 * cR + m11 * cG + m12 * cB)>>shift;
     int b = (m20 * cR + m21 * cG + m22 * cB)>>shift;
-    //assert_(r>=0 && r<1<<15 && b>=0 && b<1<<15 && g>=0 && g<1<<15, r, g, b);
+    int Y = (m30 * cR + m31 * cG + m32 * cB)>>shift;
     assert_(r>=-(1<<15) && r<1<<15 && b>=-(1<<15) && b<1<<15 && g>=-(1<<15) && g<1<<15, r, g, b);
-    //histogram[::max(0, r+g+b)/3]++; // FIXME: brightness?
-    R(X, Y) = r;
-    G(X, Y) = g;
-    B(X, Y) = b;
+    assert_(Y >= -6 && Y < 1<<15, Y);
+    histogram[::max(0,Y)]++;
+    R(tX, tY) = r;
+    G(tX, tY) = g;
+    B(tX, tY) = b;
    }
   }
-  //uint bin = 0; for(uint count =0; count < size_t(size.x*size.y/2); bin++) count += histogram[bin];
-  //log(bin, log2(bin/float(1<<15)), float(1<<11)/bin, log2(float(1<<11)/bin));
+  uint bin = 0; for(uint count =0; count < size_t(size.x*size.y/2); bin++) count += histogram[bin];
   Image sRGB (size);
   for(size_t i: range(sRGB.ref::size)) {
    // Scales median to -4bit
-   //uint r = (uint(R[i]) << 8) / bin; // 12-4
-   //uint g = (uint(G[i]) << 8) / bin;
-   //uint b = (uint(B[i]) << 8) / bin;
-   uint r = uint(R[i]) >> 2;
-   uint g = uint(G[i]) >> 2;
-   uint b = uint(B[i]) >> 2;
+   uint r = (uint(R[i]) << (12-4)) / bin;
+   uint g = (uint(G[i]) << (12-4)) / bin;
+   uint b = (uint(B[i]) << (12-4)) / bin;
    sRGB[i] = byte4(
       sRGB_forward[::min(b, 0xFFFu)],
       sRGB_forward[::min(g, 0xFFFu)],
