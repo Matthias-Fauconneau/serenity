ac636c27d28e54107ee25318b7e920124b1c0d0f
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2016-07-28 09:53:22 +0000

    Fixes.

--- convert.cc
+++ convert.cc
@@ -9,8 +9,7 @@ struct Convert {
   CR2 cr2 = CR2( Map(name) );
   const Image16& image = cr2.image;
   constexpr size_t cropY = 18, cropX = 96;
-  int max = 4000;
-  int min = max;
+  int max = 4000; int min = max;
   for(size_t y: range(cropY, (image.height-cropY))) for(size_t x: range(cropX, (image.width-cropX))) {
    min = ::min<int>(min, image(x,y));
    max = ::max<int>(max, image(x,y));
@@ -25,18 +24,19 @@ struct Convert {
     vec3(0.3576, 0.7152, 0.1192),
     vec3(0.1805, 0.0722, 0.9505) };
   uint minBalance = ::min(::min(cr2.whiteBalance.R, cr2.whiteBalance.G), cr2.whiteBalance.B);
-  vec3 whiteBalance ( (float)cr2.whiteBalance.R/minBalance, (float)cr2.whiteBalance.G/minBalance, (float)cr2.whiteBalance.B/minBalance );
+  vec3 whiteBalance ( (float)cr2.whiteBalance.R/minBalance, (float)cr2.whiteBalance.G/minBalance/2, (float)cr2.whiteBalance.B/minBalance );
   mat3 camRGB = camXYZ*XYZrgb;
-  for(int i: range(3)) { // Normalizes so that camRGB * 1 is 1
+  if(1) for(int i: range(3)) { // Normalizes so that camRGB * 1 is 1
    float sum = 0;
    for(int j: range(3)) sum += camRGB(i, j);
    for(int j: range(3)) camRGB(i, j) /= sum;
   }
-  mat3 m = mat3(whiteBalance) * camRGB.inverse() * mat3(vec3(1,1./2,1));
-  for(float c: m.data) assert_(c < (1<<(31-12-12)));
-  int32 m00 = 0xFFF*m(0,0), m01 = 0xFFF*m(0, 1), m02 = 0xFFF*m(0, 2);
-  int32 m10 = 0xFFF*m(1,0), m11 = 0xFFF*m(1, 1), m12 = 0xFFF*m(1, 2);
-  int32 m20 = 0xFFF*m(2,0), m21 = 0xFFF*m(2, 1), m22 = 0xFFF*m(2, 2);
+  mat3 m = camRGB.inverse() * mat3(whiteBalance*(0xFFF/float(max-min)));
+  constexpr int shift = 31-12-3; float scale = 1<<shift;
+  for(float c: m.data) assert_(c < 1<<3);
+  int32 m00 = scale*m(0,0), m01 = scale*m(0, 1), m02 = scale*m(0, 2);
+  int32 m10 = scale*m(1,0), m11 = scale*m(1, 1), m12 = scale*m(1, 2);
+  int32 m20 = scale*m(2,0), m21 = scale*m(2, 1), m22 = scale*m(2, 2);
 
   int2 size ((image.width-cropX)/2, (image.height-cropY)/2);
   Image sRGB (size);
@@ -44,12 +44,12 @@ struct Convert {
    for(size_t X: range(size.x)) {
     size_t y = cropY+2*Y, x = cropX+2*X;
     int r = image(x+0,y+0), g1 = image(x+1,y+0), g2 = image(x+0,y+1), b = image(x+1,y+1);
-    int cR = r-min;
-    int cG = g1+g2-2*min;
-    int cB = b-min;
-    int R = ::clamp(0, (m00 * cR + m01 * cG + m02 * cB)/(max-min), 0xFFF);
-    int G = ::clamp(0, (m10 * cR + m11 * cG + m12 * cB)/(max-min), 0xFFF);
-    int B = ::clamp(0, (m20 * cR + m21 * cG + m22 * cB)/(max-min), 0xFFF);
+    int32 cR = r-min;
+    int32 cG = g1+g2-2*min;
+    int32 cB = b-min;
+    int R = ::min((m00 * cR + m01 * cG + m02 * cB)>>shift, 0xFFF);
+    int G = ::min((m10 * cR + m11 * cG + m12 * cB)>>shift, 0xFFF);
+    int B = ::min((m20 * cR + m21 * cG + m22 * cB)>>shift, 0xFFF);
     sRGB(X, Y) = byte4(sRGB_forward[B], sRGB_forward[G], sRGB_forward[R], 0xFF);
    }
   }
