67d556abd938ce8ace4e1e80c30872034afec0d0
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2016-07-31 14:13:49 +0000

    Histogram equalization.

--- convert.cc
+++ convert.cc
@@ -37,36 +37,66 @@ struct Convert {
    for(int j: range(3)) sensor_RGB(i, j) = sensor_RGB(i, j) * factor;
   }
   mat3 RGB_sensor = sensor_RGB.inverse();
-  constexpr int shift = 31-15-7; float scale = 1<<shift;
-  for(float c: RGB_sensor.data) assert_(c < 1<<7);
+  mat3 XYZ_sensor = XYZ_RGB*RGB_sensor; // FIXME: direct WB normalization in XYZ ?
+  /*for(float c: RGB_sensor.data) assert_(c < 1<<5);
+  constexpr int shift = 31-13-5; float scale = 1<<shift;
   int32 m00 = scale*RGB_sensor(0,0), m01 = scale*RGB_sensor(0, 1), m02 = scale*RGB_sensor(0, 2); // R
   int32 m10 = scale*RGB_sensor(1,0), m11 = scale*RGB_sensor(1, 1), m12 = scale*RGB_sensor(1, 2); // G
   int32 m20 = scale*RGB_sensor(2,0), m21 = scale*RGB_sensor(2, 1), m22 = scale*RGB_sensor(2, 2); // B
-  int32 m30 = XYZ_RGB(1, 0)*(m00+m10+m20), m31 = XYZ_RGB(1, 0)*(m01+m11+m21), m32 = XYZ_RGB(1, 0)*(m02+m12+m22); // Y
-  buffer<uint32> histogram (1<<15); // 128K
+  int32 m30 = XYZ_RGB(1, 0)*(m00+m10+m20), m31 = XYZ_RGB(1, 0)*(m01+m11+m21), m32 = XYZ_RGB(1, 0)*(m02+m12+m22); // Y*/
+  buffer<uint32> histogram (1<<14); // 128K
   histogram.clear(0);
   int2 size ((image.width-cropX)/2, (image.height-cropY)/2);
-  Image16 R(size), G(size), B(size);
+  //Image16 R(size), G(size), B(size);
+  uint maxY = 0;
   for(size_t tY: range(size.y)) {
    for(size_t tX: range(size.x)) {
     size_t y = cropY+2*tY, x = cropX+2*tX; // crop/2*2
     int32 cR = image(x+0,y+0)-min;
     int32 cG = image(x+1,y+0)+image(x+0,y+1)-2*min;
     int32 cB = image(x+1,y+1)-min;
-    assert_(cR>=0 && cR<1<<15 && cG>=-1 && cG<1<<15 && cB>=-1 && cB<1<<15, cR, cG, cB, tX, tY, x, y);
-    int r = (m00 * cR + m01 * cG + m02 * cB)>>shift;
+    assert_(cR>=0 && cR<1<<13 && cG>=0 && cG<1<<13 && cB>=0 && cB<1<<13, cR, cG, cB, tX, tY, x, y);
+    /*int r = (m00 * cR + m01 * cG + m02 * cB)>>shift;
     int g = (m10 * cR + m11 * cG + m12 * cB)>>shift;
     int b = (m20 * cR + m21 * cG + m22 * cB)>>shift;
     int Y = (m30 * cR + m31 * cG + m32 * cB)>>shift;
-    assert_(r>=-(1<<15) && r<1<<15 && g>=-(1<<15) && g<1<<15 && b>=-(1<<15) && b<1<<15, r, g, b);
-    assert_(Y >= -8 && Y < 1<<15, Y);
-    histogram[::max(0,Y)]++;
-    R(tX, tY) = r;
+    assert_(r>=0 && r<1<<15 && g>=0 && g<1<<15 && b>=0 && b<1<<15, r, g, b);*/
+    int Y = (XYZ_sensor * vec3(cR, cG, cB))[1];
+    maxY = ::max<uint>(maxY, Y);
+    assert_(Y >= 0 && Y < 1<<14, Y);
+    histogram[Y]++;
+    /*R(tX, tY) = r;
     G(tX, tY) = g;
-    B(tX, tY) = b;
+    B(tX, tY) = b;*/
    }
   }
-  uint bin = 0; for(uint count =0; count < size_t(size.x*size.y/2); bin++) count += histogram[bin];
+
+  buffer<uint16> HE (maxY+1); // 128K
+  const uint totalCount = size.x*size.y;
+  const uint targetMaxY = 1<<12;
+  for(uint bin=0, count=0; bin < maxY; bin++) {
+   HE[bin] = (uint64)count*targetMaxY/totalCount;
+   count += histogram[bin];
+  }
+
+  Image16 R(size), G(size), B(size);
+  mat3 RGB_XYZ = XYZ_RGB.inverse();
+  for(size_t tY: range(size.y)) {
+   for(size_t tX: range(size.x)) {
+    size_t y = cropY+2*tY, x = cropX+2*tX; // crop/2*2
+    int32 cR = image(x+0,y+0)-min;
+    int32 cG = image(x+1,y+0)+image(x+0,y+1)-2*min;
+    int32 cB = image(x+1,y+1)-min;
+    vec3 XYZ = XYZ_sensor * vec3(cR, cG, cB);
+    int32 Y = XYZ.y;
+    int32 HEy = HE[Y];
+    float HEscale = float(HEy)/float(Y);
+    rgb3f RGB = RGB_XYZ * (HEscale * XYZ);
+    R(tX, tY) = RGB.r;
+    G(tX, tY) = RGB.g;
+    B(tX, tY) = RGB.b;
+   }
+  }
 
 #if 0
   // Lens distortion correction
@@ -95,10 +125,9 @@ struct Convert {
 
   Image sRGB (size);
   for(size_t i: range(sRGB.ref::size)) {
-   // Scales median to -4bit
-   uint r = (uint(R[i]) << (12-4)) / bin;
-   uint g = (uint(G[i]) << (12-4)) / bin;
-   uint b = (uint(B[i]) << (12-4)) / bin;
+   uint r = R[i];
+   uint g = G[i];
+   uint b = B[i];
    sRGB[i] = byte4(
       sRGB_forward[::min(b, 0xFFFu)],
       sRGB_forward[::min(g, 0xFFFu)],
