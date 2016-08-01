f688a9ad1516d7d39aea05f5cc0ec8623d76c666
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2016-08-01 13:00:41 +0000

    Approximate (Interpolated) Adaptive Histogram Equalization.

--- convert.cc
+++ convert.cc
@@ -10,7 +10,11 @@ struct Convert {
   CR2 cr2 = CR2( Map(name) );
   const Image16& image = cr2.image;
 
-  constexpr size_t cropY = 18, cropX = 96;
+  const int minCropX = 96, minCropY = 18;
+  const int fullX = image.size.x-minCropX, fullY = image.size.y-minCropY;
+  constexpr int N = 256;
+  const int cropX = minCropX+fullX-(fullX/(3*2*N)*(3*2*N)), cropY = minCropY+fullY-(fullY/(2*2*N)*(2*2*N));
+  assert_(cropX >= minCropX && cropX <= fullX/5 && cropY >= minCropY && cropY <= fullY/5, fullX, fullY, cropX, cropY, fullX/cropX, fullY/cropY);
 
   // Min/Max
   uint min = -1, max = 0;
@@ -23,16 +27,20 @@ struct Convert {
   // "Demosaic" (G1+G2) (and de-bias black level)
   int2 size ((image.size.x-cropX)/2, (image.size.y-cropY)/2);
   Image16 R(size), G(size), B(size);
+  const size_t bStride = image.stride, stride = R.stride;
   for(size_t y: range(size.y)) {
+   const size_t bRowI = (cropY+2*y)*bStride + cropX; // crop/2*2
+   const size_t rowI = y*stride;
    for(size_t x: range(size.x)) {
-    size_t bY = cropY+2*y, bX = cropX+2*x; // crop/2*2
-    int32 r = image(bX+0,bY+0)-min;
-    int32 g = image(bX+1,bY+0)+image(bX+0,bY+1)-2*min;
-    int32 b = image(bX+1,bY+1)-min;
-    assert_(r>=0 && r<1<<13 && g>=0 && g<1<<13 && b>=0 && b<1<<13, r, g, b, x, y, x, y);
-    R(x, y) = r;
-    G(x, y) = g;
-    B(x, y) = b;
+    const size_t bI = bRowI + 2*x;
+    const int32 r = image[bI]-min;
+    const int32 g = image[bI+1]+image[bI+bStride]-2*min;
+    const int32 b = image[bI+bStride+1]-min;
+    assert_(r>=0 && r<1<<13 && g>=0 && g<1<<13 && b>=0 && b<1<<13, r, g, b, x, y);
+    const size_t i = rowI + x;
+    R[i] = r;
+    G[i] = g;
+    B[i] = b;
    }
   }
 
@@ -115,7 +123,7 @@ struct Convert {
    G[i] = int(RGB.g);
    B[i] = int(RGB.b);
   }
-#else
+#elif 0
   // Luminance image
   uint maxY = 0;
   Image16 Y(size);
@@ -146,7 +154,7 @@ struct Convert {
     assert_(s < sq(2*L+1)*2, s, sq(2*L+1)*2);
     vec3 XYZ = XYZ_sensor * vec3(R[i], G[i], B[i]);
     int32 y = XYZ.y;
-    int32 HEy = s*(1<<13)/(sq(2*L+1)*2);
+    int32 HEy = s*(1<<11)/(sq(2*L+1)*2);
     float HEscale = float(HEy)/float(y);
     rgb3f RGB = RGB_XYZ * (HEscale * XYZ);
     assert_(int3(vec3(RGB))<int3(8192), RGB);
@@ -156,6 +164,73 @@ struct Convert {
    }
   }
   log(AHE);
+#else
+  assert_(2*size.x == 3*size.y && size.x%N == 0 && size.y%N == 0, size,  size.x%N, size.y%N, image.size, cropX, cropY);
+  const int maxY = 1<<15;
+  struct HE { uint16 HE[maxY]; };
+  ImageT<HE> HEs (size/N); // 20 MB
+  for(size_t Y: range(size.y/N)) {
+   for(size_t X: range(size.x/N)) {
+    // Luminance histogram
+    buffer<uint16> histogram (maxY); // 64K
+    histogram.clear(0);
+    const size_t tileI = Y*N*stride + X*N;
+    for(int y: range(N)) {
+     const size_t rowI = tileI + y*stride;
+     for(int x: range(N)) {
+      const size_t i = rowI + x;
+      int Y = (XYZ_sensor * vec3(R[i], G[i], B[i]))[1];
+      assert_(Y >= 0 && Y < maxY, Y);
+      histogram[Y]++;
+     }
+    }
+
+    // Evaluates histogram equalization
+    HE& HE = HEs(X, Y);
+    constexpr uint totalCount = N*N;
+    constexpr uint targetMaxY = 1<<11;
+    for(uint bin=0, count=0; bin < maxY; bin++) {
+     HE.HE[bin] = (uint64)count*targetMaxY/totalCount;
+     count += histogram[bin];
+    }
+   }
+  }
+
+  // Colorspace conversion, Histogram equalization
+  mat3 RGB_XYZ = XYZ_RGB.inverse();
+  for(size_t Y: range(size.y/N-1)) {
+   for(size_t X: range(size.x/N-1)) {
+    const size_t tileI = (Y*N+N/2)*stride + (X*N+N/2);
+    const HE HE00 = HEs(X+0, Y+0);
+    const HE HE10 = HEs(X+1, Y+0);
+    const HE HE01 = HEs(X+0, Y+1);
+    const HE HE11 = HEs(X+1, Y+1);
+    for(int y: range(N)) {
+     const size_t rowI = tileI + y*stride;
+     const float fy = float(y)/N;
+     for(int x: range(N)) {
+      const size_t i = rowI + x;
+      const vec3 XYZ = XYZ_sensor * vec3(R[i], G[i], B[i]);
+      const int32 Y = XYZ.y;
+      const int32 HE00y = HE00.HE[Y];
+      const int32 HE01y = HE01.HE[Y];
+      const int32 HE10y = HE10.HE[Y];
+      const int32 HE11y = HE11.HE[Y];
+      // FIXME: 5 fmul, 5 cvt -> 4 imul, 2 shift, 1 cvt ?
+      const float fx = float(x)/N;
+      const float HEy =
+        (1-fy) * ( (1-fx) * float(HE00y) + fx * float(HE10y) ) +
+        fy * ( (1-fx) * float(HE01y) + fx * float(HE11y) );
+      const float HEscale = float(HEy)/XYZ.y;
+      const rgb3f RGB = RGB_XYZ * (HEscale * XYZ);
+      assert_(int3(vec3(RGB))<int3(8192), RGB, HEy, HEscale, fx, fy, HE00y, HE10y, HE01y, HE11y);
+      R[i] = int(RGB.r);
+      G[i] = int(RGB.g);
+      B[i] = int(RGB.b);
+     }
+    }
+   }
+  }
 #endif
 
   // Gamma compression
