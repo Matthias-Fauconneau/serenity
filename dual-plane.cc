3d1785f552be94175bae28a58922f38a28d4a17c
Matthias Fauconneau <Matthias.Fauconneau@gmail.com>
2017-01-11 15:11:38 +0100

    Fix view transform.

--- dual-plane.cc
+++ dual-plane.cc
@@ -22,7 +22,7 @@ struct ViewApp : ViewControl {
     Map map;
     ref<half> field;
 
-    bool orthographic = true;
+    bool orthographic = false;
     bool depthCorrect = true;
     //bool uniformWeights = false;
     bool wideReconstruction = true;
@@ -38,7 +38,9 @@ struct ViewApp : ViewControl {
     struct {
         ImageF sumB, sumG, sumR;
         uint count = 0; // Iteration count (Resets on uv change)
-        vec2 uv = 0;
+        int2 target = 0;
+        //vec2 st = 0, uv_uncorrected = 0;
+        //vec2 xyz = 0;
     } st;
 
     ViewApp() {
@@ -50,6 +52,7 @@ struct ViewApp : ViewControl {
         window->actions[Key('d')] = [this]{ depthCorrect=!depthCorrect; window->render(); };
         //window->actions[Key('u')] = [this]{ uniformWeights=!uniformWeights; window->render(); window->setTitle(str(uniformWeights)); };
         window->actions[Key('w')] = [this]{ wideReconstruction=!wideReconstruction; window->render(); window->setTitle(str(wideReconstruction)); };
+        st.target = 960/2;
     }
     void load(string name) {
         field = {};
@@ -98,6 +101,11 @@ struct ViewApp : ViewControl {
             newCamera[3].xyz() = (mat3)newCamera * vec3(0, 0, -2);
         } else {
             s = angles.x/(PI/3), t = angles.y/(PI/3);
+            vec4 O = newCamera * vec4(s, t, 0, 0);
+            newCamera.translate(vec3(s, t, 0));
+            newCamera(0, 2) -= O.x;
+            newCamera(1, 2) -= O.y;
+            newCamera(2, 2) -= O.z;
         }
 #if 1
         {
@@ -112,13 +120,14 @@ struct ViewApp : ViewControl {
             }
             count++;
 
-            parallel_chunk(target.size.y, [this, camera, newCamera, s, t, &Z, &target](uint _threadId, uint start, uint sizeI) {
+            parallel_chunk(target.size.y, [this, camera, newCamera, /*s, t,*/ &Z, &target](uint _threadId, uint start, uint sizeI) {
                 TraceBase tracer(scene, _threadId);
                 for(int y: range(start, start+sizeI)) for(uint x: range(target.size.x)) {
-                    const vec3 O = newCamera * vec3(s, t, 0);
+                    //const vec3 O = newCamera * vec3(s, t, 0);
+                    const vec3 O = newCamera * vec3(0, 0, 0);
                     const vec3 P = newCamera * vec3((2.f*x/float(target.size.x-1)-1), ((2.f*y/float(target.size.y-1)-1)), 1); // FIXME: sheared perspective
                     float hitDistance;
-                    Vec3f emission = tracer.trace(O, P, hitDistance, count);
+                    Vec3f emission = tracer.trace(O, P, hitDistance, /*count*/8);
                     size_t i = y*target.size.x+x;
                     sumR[i] += emission[0];
                     sumG[i] += emission[1];
@@ -139,7 +148,7 @@ struct ViewApp : ViewControl {
         if(imageSize && imageCount) {
             const Image target = cropShare(window, int2(imageSize.x,0), imageSize);
             assert_(imageCount.x == imageCount.y);
-            parallel_chunk(target.size.y, [this, &target, camera, newCamera, s, t, &Z](uint, uint start, uint sizeI) {
+            parallel_chunk(target.size.y, [this, &target, camera, newCamera, /*s, t,*/ &Z](uint, uint start, uint sizeI) {
                 const uint size1 = imageSize.x *1;
                 const uint size2 = imageSize.y *size1;
                 const uint size3 = imageCount.x*size2;
@@ -158,13 +167,14 @@ struct ViewApp : ViewControl {
                   fieldG {imageCount, imageSize, field.slice(2*size4, size4)},
                   fieldR {imageCount, imageSize, field.slice(3*size4, size4)};
                 assert_(imageSize.x%2==0); // Gather 32bit / half
-                const unused v2ui sample2D = {    0,           size1/2};
+                const v2ui sample2D = {    0,           size1/2};
                 const v8ui sample4D = {    0,           size1/2,         size2/2,       (size2+size1)/2,
                                            size3/2, (size3+size1)/2, (size3+size2)/2, (size3+size2+size1)/2};
                 const float scale = (float)(imageSize.x-1) / (imageCount.x-1); // st -> uv
                 for(int targetY: range(start, start+sizeI)) for(int targetX: range(target.size.x)) {
                     size_t targetIndex = (target.size.y-1-targetY)*target.stride + targetX;
-                    const vec3 Ow = newCamera * vec3(s, t, 0);
+                    //const vec3 Ow = newCamera * vec3(s, t, 0);
+                    const vec3 Ow = newCamera * vec3(0, 0, 0);
                     const vec3 Pw = newCamera * vec3((2.f*targetX/float(target.size.x-1)-1), ((2.f*targetY/float(target.size.y-1)-1)), 1);
                     const vec3 O = camera.inverse() * Ow;
                     const vec3 P = camera.inverse() * Pw;
@@ -301,13 +311,97 @@ struct ViewApp : ViewControl {
                 if(st.sumR.size != imageCount) st.sumR = ImageF(imageCount); st.sumR.clear(0);
             }
             st.count++;
-            TraceBase tracer(scene, 0); // TODO: parallel
-            for(int tIndex: range(imageCount.y)) for(int sIndex: range(imageCount.x)) {
+            //TraceBase tracer(scene, 0); // TODO: parallel
+            {
+                const uint size1 = imageSize.x *1;
+                const uint size2 = imageSize.y *size1;
+                const uint size3 = imageCount.x*size2;
+                const uint64 size4 = uint64(imageCount.y)*uint64(size3);
+                const struct Image4DH : ref<half> {
+                    uint4 size;
+                    Image4DH(uint2 imageCount, uint2 imageSize, ref<half> data) : ref<half>(data), size(imageCount.y, imageCount.x, imageSize.y, imageSize.x) {}
+                    const half& operator ()(uint s, uint t, uint u, uint v) const {
+                        assert_(t < size[0] && s < size[1] && v < size[2] && u < size[3], int(s), int(t), int(u), int(v));
+                        size_t index = ((uint64(t)*size[1]+s)*size[2]+v)*size[3]+u;
+                        assert_(index < ref<half>::size, int(index), ref<half>::size, int(s), int(t), int(u), int(v), size);
+                        return operator[](index);
+                    }
+                } fieldZ {imageCount, imageSize, field.slice(0*size4, size4)},
+                  fieldB {imageCount, imageSize, field.slice(1*size4, size4)},
+                  fieldG {imageCount, imageSize, field.slice(2*size4, size4)},
+                  fieldR {imageCount, imageSize, field.slice(3*size4, size4)};
+                assert_(imageSize.x%2==0); // Gather 32bit / half
+                const v2ui sample2D = {    0,           size1/2};
+                const float scale = (float)(imageSize.x-1) / (imageCount.x-1); // st -> uv
+                //const vec3 Ow = newCamera * vec3(s, t, 0);
+                const vec3 Ow = newCamera * vec3(0, 0, 0);
+                const vec3 Pw = newCamera * vec3((2.f*st.target.x/float(imageSize.x-1)-1), ((2.f*st.target.y/float(imageSize.y-1)-1)), 1);
+                {
+                    const vec3 O = camera.inverse() * Ow;
+                    const vec3 P = camera.inverse() * Pw;
+                    const vec3 d = normalize(P-O);
+
+                    const vec3 n (0,0,-1);
+                    const float nd = dot(n, d);
+                    const vec3 n_nd = n / nd;
+
+                    const vec2 Pst = O.xy() + dot(n_nd, vec3(0,0,0)-O) * d.xy();
+                    const vec2 ST = (Pst+vec2(1))/2.f;
+                    const vec2 Puv = P.xy() + dot(n_nd, vec3(0,0,1)-P) * d.xy();
+                    const vec2 UV = (Puv+vec2(1))/2.f;
+
+                    const vec2 st = vec2(0x1p-16) + vec2(1-0x1p-16) * ST * vec2(imageCount-uint2(1));
+                    const vec2 uv_uncorrected = vec2(1-0x1p-16) * UV * vec2(imageSize-uint2(1));
+
+                    const float z = Z(this->st.target.x, this->st.target.y);
+                    const float z_ = z==inff ? 1 : (z-1)/z;
+                    const float zW = z * length(Pw-Ow);
+
+                    for(int tIndex: range(imageCount.y)) for(int sIndex: range(imageCount.x)) {
+                        vec2 uv_ = uv_uncorrected + scale * (vec2(sIndex,tIndex)-st) * z_;
+                        float b = 0, g = 0, r = 0;
+                        if(!(uv_[0] < 0 || uv_[1] < 0)) {
+                            int uIndex = uv_[0], vIndex = uv_[1];
+                            if(!(uIndex >= int(imageSize.x)-1 || vIndex >= int(imageSize.y)-1)) {
+                                const size_t base = (size_t)(tIndex)*size3 + (sIndex)*size2 + vIndex*size1 + uIndex;
+                                const v2sf x = {uv_[1], uv_[0]}; // vu
+                                const v4sf X = __builtin_shufflevector(x, x, 0,1, 0,1);
+                                static const v4sf _0011f = {0,0,1,1};
+                                const v4sf w_1mw = abs(X - floor(X) - _0011f); // fract(x), 1-fract(x)
+                                const v4sf Z = toFloat((v4hf)gather((float*)(fieldZ.data+base), sample2D)); // FIXME
+#if 0
+                                const v4sf w01uv = and( Z==float4(z)/*inf=inf*/ | abs(Z - float4(zW)) < float4(0x1p-2*length(Pw-Ow)), // Discards far samples (tradeoff between edge and anisotropic accuracy)
+                                                        __builtin_shufflevector(w_1mw, w_1mw, 2,2,0,0)    // vvVV
+                                                        * __builtin_shufflevector(w_1mw, w_1mw, 3,1,3,1) ); // uUuU
+#else
+                                const v4sf w01uv = __builtin_shufflevector(w_1mw, w_1mw, 2,2,0,0)  // vvVV
+                                                 * __builtin_shufflevector(w_1mw, w_1mw, 3,1,3,1); // uUuU
+#endif
+                                const float sum = ::hsum(w01uv);
+                                if(sum) {
+                                    const v4sf w01 = float4(1./sum) * w01uv; // Renormalizes uv interpolation (in case of discarded samples)
+                                    b = dot(w01, toFloat((v4hf)gather((float*)(fieldB.data+base), sample2D)));
+                                    g = dot(w01, toFloat((v4hf)gather((float*)(fieldG.data+base), sample2D)));
+                                    r = dot(w01, toFloat((v4hf)gather((float*)(fieldR.data+base), sample2D)));
+                                }
+                            }
+                        }
+                        const uint R = 0xFFF*::min(1.f, r);
+                        const uint G = 0xFFF*::min(1.f, g);
+                        const uint B = 0xFFF*::min(1.f, b);
+                        extern uint8 sRGB_forward[0x1000];
+                        target[(tIndex*2+0)*target.stride+sIndex*2+0] = target[(tIndex*2+0)*target.stride+sIndex*2+1] =
+                        target[(tIndex*2+1)*target.stride+sIndex*2+0] = target[(tIndex*2+1)*target.stride+sIndex*2+1] = byte4(sRGB_forward[B], sRGB_forward[G], sRGB_forward[R], 0xFF);
+                    }
+                }
+            }
+            /*for(int tIndex: range(imageCount.y)) for(int sIndex: range(imageCount.x)) {
                 const float s = 2*(sIndex/float(imageCount.x-1))-1, t = 2*(tIndex/float(imageCount.y-1))-1;
                 const vec3 O = newCamera * vec3(s, t, 0);
-                const vec3 P = newCamera * vec3(st.uv, 1);
+                const vec3 P = newCamera * vec3(st.uv, 1); // TODO: uv correction
                 float hitDistance;
-                Vec3f emission = tracer.trace(O, P, hitDistance, 16/*::max(16, st.count)*/);
+                Vec3f emission = tracer.trace(O, P, hitDistance, 16);
+                float Z = hitDistance / ::length(P-O);
                 size_t i = tIndex*imageCount.x+sIndex;
                 st.sumR[i] += emission[0];
                 st.sumG[i] += emission[1];
@@ -319,12 +413,48 @@ struct ViewApp : ViewControl {
                 extern uint8 sRGB_forward[0x1000];
                 target[(tIndex*2+0)*target.stride+sIndex*2+0] = target[(tIndex*2+0)*target.stride+sIndex*2+1] =
                 target[(tIndex*2+1)*target.stride+sIndex*2+0] = target[(tIndex*2+1)*target.stride+sIndex*2+1] = byte4(sRGB_forward[b], sRGB_forward[g], sRGB_forward[r], 0xFF);
-            }
+            }*/
         }
     }
     virtual bool mouseEvent(vec2 cursor, vec2 size, Event event, Button button, Widget*& widget) override {
         if(event == Press) {
-            st.uv = vec2((2.f*cursor.x/float(imageSize.x-1)-1), -((2.f*cursor.y/float(imageSize.y-1)-1)));
+            st.target = int2(cursor.x, 960-1-cursor.y);
+            /*const mat4 camera = parseCamera(readFile("scene.json"));
+            mat4 newCamera = camera;
+            float s = 0, t = 0;
+            if(orthographic) {
+                newCamera.rotateY(angles.x); // Yaw
+                newCamera.rotateX(angles.y); // Pitch
+                newCamera[3].xyz() = (mat3)newCamera * vec3(0, 0, -2);
+            } else {
+                s = angles.x/(PI/3), t = angles.y/(PI/3);
+            }
+
+            {
+                const vec3 Ow = newCamera * vec3(s, t, 0);
+                const vec3 Pw = newCamera * vec3((2.f*targetX/float(target.size.x-1)-1), ((2.f*targetY/float(target.size.y-1)-1)), 1);
+                const vec3 O = camera.inverse() * Ow;
+                const vec3 P = camera.inverse() * Pw;
+                const vec3 d = normalize(P-O);
+
+                const vec3 n (0,0,-1);
+                const float nd = dot(n, d);
+                const vec3 n_nd = n / nd;
+
+                const vec2 Pst = O.xy() + dot(n_nd, vec3(0,0,0)-O) * d.xy();
+                const vec2 ST = (Pst+vec2(1))/2.f;
+                const vec2 Puv = P.xy() + dot(n_nd, vec3(0,0,1)-P) * d.xy();
+                const vec2 UV = (Puv+vec2(1))/2.f;
+
+                st.st = vec2(0x1p-16) + vec2(1-0x1p-16) * ST * vec2(imageCount-uint2(1));
+                st.uv_uncorrected = vec2(1-0x1p-16) * UV * vec2(imageSize-uint2(1));
+            }*/
+            /*const vec3 O = newCamera * vec3(s, t, 0);
+            const vec3 P = newCamera * vec3((2.f*x/float(target.size.x-1)-1), ((2.f*y/float(target.size.y-1)-1)), 1); // FIXME: sheared perspective
+            float hitDistance;
+            tracer.trace(O, P, hitDistance, 0);
+            st.xyz = O + hitDistance*normalize(P-O);
+            //st.uv = vec2((2.f*cursor.x/float(imageSize.x-1)-1), -((2.f*cursor.y/float(imageSize.y-1)-1)));*/
             st.count = 0;
         }
         return ViewControl::mouseEvent(cursor, size, event, button, widget);
